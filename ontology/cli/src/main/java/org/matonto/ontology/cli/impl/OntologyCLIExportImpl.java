bdf93f3043255db9f82f3951c995529ef1f13798
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2015-10-30 15:26:03 -0400

    Added ontology cli

---
+++ ontology/cli/src/main/java/org/matonto/ontology/cli/impl/OntologyCLIExportImpl.java
@@ -0,0 +1,130 @@
+package org.matonto.ontology.cli.impl;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Optional;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.matonto.ontology.core.api.Ontology;
+import org.matonto.ontology.core.api.OntologyManager;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
+import aQute.bnd.annotation.component.Reference;
+
+
+@Component (immediate=true)
+@Command(scope = "matonto", name = "exportOntology", description="Exports ontology from a repository")
+public class OntologyCLIExportImpl  extends OsgiCommandSupport
+{
+	private OntologyManager manager;
+	private static final Logger LOG = LoggerFactory.getLogger(OntologyCLIExportImpl.class);
+	
+	
+    @Activate
+    public void activate() 
+    {
+        LOG.info("Activating the OntologyCLIExportImpl");
+    }
+ 
+    @Deactivate
+    public void deactivate() 
+    {
+        LOG.info("Deactivating the OntologyCLIExportImpl");
+    }
+    
+	@Reference
+	protected void setOntologyManager(OntologyManager manager)
+	{
+		this.manager = manager;
+	}
+	
+	//Command Line Arguments and Options	
+	@Argument(index = 0, name = "RepositoryID", description = "The id of the repository the file will be exported from", required = true, multiValued = false)
+	String repositoryId = null;
+	
+	@Argument(index = 1, name = "namespace", description = "The namespace of the context id for the ontology named graph to be exported", required = true, multiValued = false)
+	String namespace = null;
+	
+	@Argument(index = 2, name = "localName", description = "The local name of the context id for the ontology named graph to be exported", required = true, multiValued = false)
+	String localName = null;
+	
+	@Argument(index = 3, name = "DataFormat", description = "The data format of the ontology being exported. Supported data formats are: RDF(rdf/xml), OWL(owl/xml) and TURTLE (default type)", required = true)
+	String dataFormat = null;
+	
+	@Argument(index = 4, name = "exportToFile", description = "The file where exported ontology to be stored", required = true, multiValued = false)
+	String toFile = null;
+	
+	
+	@Override
+	protected Object doExecute() throws Exception 
+	{
+		boolean exported = false;
+		exported = exportOntology();
+		
+		if(exported)
+			System.out.println("Succesful export to file.");
+		else
+			System.out.println("Unsuccessful export to file.");
+
+		return null;
+	}
+	
+
+	
+	private boolean exportOntology()
+	{	
+		boolean exported = false;		
+		OutputStream outputStream = null;
+	
+		try {
+			System.out.println("Exporting ontology to file...");
+			File newFile = new File(toFile);
+				
+			if (!newFile.exists())
+				newFile.createNewFile();
+			
+			URI uri = new URIImpl(namespace + "#" + localName);
+			Optional<Ontology> ontology = manager.retrieveOntology(uri);
+			
+			if(ontology.isPresent()) 
+			{
+				if(dataFormat.equalsIgnoreCase("rdf"))
+					outputStream = ontology.get().asRdfXml();
+				
+				else if(dataFormat.equalsIgnoreCase("owl"))
+					outputStream = ontology.get().asOwlXml();
+				
+				else
+					outputStream = ontology.get().asTurtle();
+				
+				BufferedWriter bw = new BufferedWriter(new FileWriter(newFile.getAbsoluteFile()));
+				bw.write(outputStream.toString());
+				bw.flush();
+				bw.close();
+				exported = true;
+			}
+			
+			
+		} catch (IOException fileError) {
+			System.out.println("Failed to create file.");
+			fileError.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(outputStream);	
+		}
+			
+		return exported;
+	}
+
+}
---
+++ ontology/cli/src/main/java/org/matonto/ontology/cli/impl/OntologyCLIImportImpl.java
@@ -0,0 +1,96 @@
+package org.matonto.ontology.cli.impl;
+
+import java.io.File;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.matonto.ontology.core.api.Ontology;
+import org.matonto.ontology.core.api.OntologyManager;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
+import aQute.bnd.annotation.component.Reference;
+
+
+@Component (immediate=true)
+@Command(scope = "matonto", name = "importOntology", description = "Imports ontology to a repository")
+public class OntologyCLIImportImpl extends OsgiCommandSupport
+{
+	private OntologyManager manager;
+	private static final Logger LOG = LoggerFactory.getLogger(OntologyCLIImportImpl.class);
+	
+    @Activate
+    public void activate() 
+    {
+        LOG.info("Activating the OntologyCLIImportImpl");
+    }
+ 
+    @Deactivate
+    public void deactivate() 
+    {
+        LOG.info("Deactivating the OntologyCLIImportImpl");
+    }
+	
+	@Reference
+	protected void setOntologyManager(OntologyManager manager)
+	{
+		this.manager = manager;
+	}
+
+	
+	//Command Line Arguments and Options	
+	@Argument(index = 0, name = "RepositoryID", description = "The id of the repository the file will be imported to", required = true, multiValued = false)
+	String repositoryId = null;
+	
+	@Argument(index = 1, name = "ImportFile", description = "The file to be imported into the repository", required = true, multiValued = false)
+	String fromFile = null;
+	
+	@Argument(index = 2, name = "namespace", description = "The namespace of the context id for ontology named graph to be created under", required = true, multiValued = false)
+	String namespace = null;
+	
+	@Argument(index = 3, name = "localName", description = "The local name of the context id for ontology named graph to be created under", required = true, multiValued = false)
+	String localName = null;
+	
+	
+	@Override
+	protected Object doExecute() throws Exception 
+	{	
+		boolean persisted = false;
+		persisted = importOntology();
+		
+		if(persisted)
+			System.out.println("Succesful file import.");
+		else
+			System.out.println("Unsuccessful file import.");
+
+		return null;
+	}
+
+	
+	private boolean importOntology()
+	{	
+		boolean persisted = false;
+
+		System.out.println("Importing ontology file...");
+
+		File newFile = new File(fromFile);
+		if(newFile.exists()) {
+			URI uri = new URIImpl(namespace + "#" + localName);
+			Ontology ontology = manager.createOntology(newFile, uri);
+			persisted = manager.storeOntology(ontology);
+		}
+				
+		else
+			System.out.println("File does not exist.");
+			
+
+		return persisted;
+	}
+
+}
