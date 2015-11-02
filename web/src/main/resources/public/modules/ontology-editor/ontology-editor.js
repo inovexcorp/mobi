7515aeadfc2224b938a902b4278838975f722b7a
Jordan Lewis <jordan.lewis@inovexcorp.com>
2015-11-02 16:54:30 -0500

    Ontology editor changes to create one single object and update it alone whenever the user does anything on the page.

--- web/src/main/resources/public/modules/ontology-editor/ontology-editor.js
+++ web/src/main/resources/public/modules/ontology-editor/ontology-editor.js
@@ -1,3 +1,10 @@
+/*
+ * Throughout this file you will see these abbreviations used repeatedly. Instead of commenting this everytime, I put them up here once.
+ * oi - ontology index
+ * ci - class index
+ * pi - property index
+ */
+
 (function() {
     'use strict';
 
@@ -11,58 +18,20 @@
         var _currentEdits = {},
             vm = this;
 
-        /*
-        - all the information needed to determine the state
-        state = {
-            ontologyIndex: int,
-            classIndex: int || empty,
-            propertyIndex: int || empty
-        }
-
-        - the four different states
-        states = {
-            everything, class, object, datatype
-        }
-        */
-
+        vm.success = false;
+        vm.shown = 'default';
+        vm.tab = 'everything';
+        vm.propertyDefault = { '@id': '' };
+        vm.classDefault = { '@id': '', '@type': 'owl:Class', properties: [] };
+        vm.ontologyDefault = { '@id': '', '@type': 'owl:Ontology', delimiter: '#', classes: [] };
+        vm.edit = edit;
+        vm.changeTab = changeTab;
+        vm.versions = [];
+        vm.ontologies = [];
         vm.state = {};
         vm.current = {};
-        vm.edit = edit;
-        vm.tab = 'everything';
-        vm.changeTab = changeTab;
-
-
-
-        vm.isEdit;
-        vm.createText = 'Create';
-        vm.editText = 'Update';
-        vm.shown = 'default';
         vm.selected = {};
-        vm.ontologies = [];
-        vm.versions = [];
-        vm.header = '';
-        vm.success = false;
-
-        /* ----- Ontology variables ----- */
-        vm.ontologyDefault = { '@id': '', '@type': 'owl:Ontology', delimiter: '#', classes: [] };
-        vm.ontology = {};
-        vm.createOntology = createOntology;
-        vm.editOntology = editOntology;
-        vm.submitOntology = submitOntology;
-
-        /* ----- Class variables ----- */
-        vm.classDefault = { '@id': '', '@type': 'owl:Class', properties: [] };
-        vm.class = {};
-        vm.createClass = createClass;
-        vm.editClass = editClass;
-        vm.submitClass = submitClass;
-
-        /* ----- Property variables ----- */
-        vm.propertyDefault = { '@id': '' };
-        vm.property = {};
-        vm.createProperty = createProperty;
-        vm.editProperty = editProperty;
-        vm.submitProperty = submitProperty;
+        vm.newItems = {};
 
         activate();
 
@@ -70,7 +39,8 @@
             _getOntologies();
         }
 
-        function _updateRefs(obj, old, fresh, isPrefix) {
+        // if the uri of the ontology changes, this function will update the rest of the ids to match
+        function _updateRefs(obj, old, fresh) {
             var prop, i, arr;
 
             // iterates over all of the properties of the object
@@ -78,16 +48,17 @@
                 if(Object.prototype.toString.call(obj[prop]) === '[object Array]') {
                     i = obj[prop].length;
                     while(i--) {
-                        _updateRefs(obj[prop][i], old, fresh, isPrefix);
+                        _updateRefs(obj[prop][i], old, fresh);
                     }
                 } else if(typeof obj[prop] === 'object') {
-                    _updateRefs(obj[prop], old, fresh, isPrefix);
+                    _updateRefs(obj[prop], old, fresh);
                 } else if(obj[prop].indexOf(old) !== -1) {
-                    obj[prop] = isPrefix ? obj[prop].replace(old, fresh) : fresh;
+                    obj[prop] = fresh + _stripPrefix(obj[prop]);
                 }
             }
         }
 
+        // takes the flattened JSON-LD data and creates our custom tree structure
         function _parseOntologies(flattened) {
             // testing speed
             var startTime = new Date().getTime();
@@ -139,12 +110,13 @@
 
             // makes it an array because it will be later
             vm.ontologies.push(ontology);
-            vm.versions.push([angular.copy(ontology)]);
+            vm.versions.push([{time: new Date(), ontology: angular.copy(ontology)}]);
 
             // updates the view with the changes made here
             $scope.$apply();
         }
 
+        // gets the ontologies and flattens them
         function _getOntologies() {
             var context = {
                     'owl': 'http://www.w3.org/2002/07/owl#',
@@ -162,6 +134,7 @@
                 });
         }
 
+        // shows the success function above the form on the right side
         function _showSuccess() {
             _saveState();
             vm.success = true;
@@ -170,27 +143,72 @@
             }, 2000);
         }
 
-        /* ----- State Functions ----- */
-        function _saveState(ontologyIndex, classIndex, propertyIndex) {
+        // saves the current state for when they change the tabs on the right
+        function _saveState(oi, ci, pi) {
             vm.state[vm.tab] = {
-                ontologyIndex: ontologyIndex,
-                classIndex: classIndex,
-                propertyIndex: propertyIndex
+                ontologyIndex: oi,
+                classIndex: ci,
+                propertyIndex: pi
             }
         }
 
-        function _setState() {
+        // removes the beginning of the @id which is referenced here by the 'id'
+        function _stripPrefix(id) {
+            var result = '',
+                hash = id.indexOf('#'),
+                slash = id.lastIndexOf('/'),
+                colon = id.lastIndexOf(':');
+            if(hash !== -1) {
+                result = id.substring(0, hash + 1);
+            } else if(slash !== -1) {
+                result = id.substring(0, slash + 1);
+            } else if(colon !== -1) {
+                result = id.substring(0, colon + 1);
+            }
+            return result;
+        }
+
+        // finds out whether they are editing or creating an object
+        function _editOrCreate(arr, index, unique) {
+            // if they are creating
+            if(index === -1) {
+                // checks to see if they were already editing this node
+                if(!vm.newItems.hasOwnProperty(unique)) vm.newItems[unique] = angular.copy(vm.propertyDefault);
+                vm.selected = vm.newItems[unique];
+            }
+            // else, they are editing
+            else {
+                var item = arr[index];
+                item.prefix = _stripPrefix(item['@id']);
+                item['@id'] = item['@id'].replace(item.prefix, '');
+                vm.selected = arr[index];
+            }
+        }
+
+        // sets the current state of the page
+        function _setState(oi, ci, pi) {
+            var arr, item, unique;
             vm.current = vm.state[vm.tab];
-            if(vm.current.propertyIndex != undefined) {
+            // if property index is specified, they are working with a property
+            if(pi != undefined) {
                 vm.shown = 'property-editor';
-            } else if(vm.current.classIndex != undefined) {
+                unique = vm.tab + vm.current.ontologyIndex + vm.current.classIndex + vm.current.propertyIndex;
+                _editOrCreate(vm.ontologies[oi].classes[ci].properties, pi, unique);
+            }
+            // else, if class index is specified, they are working with a class
+            else if(ci != undefined) {
                 vm.shown = 'class-editor';
-            } else {
+                unique = vm.tab + vm.current.ontologyIndex + vm.current.classIndex;
+                _editOrCreate(vm.ontologies[oi].classes, ci, unique);
+            }
+            // else, they have to be working with an ontology
+            else {
                 vm.shown = 'ontology-editor';
+                vm.selected = (oi === -1) ? vm.ontologyDefault : vm.ontologies[oi];
             }
         }
-        /* ----- End State Functions ----- */
 
+        // changes the form on the right depending on what tab they have selected
         function changeTab(tab) {
             vm.tab = tab;
             if(vm.state[tab]) {
@@ -200,30 +218,10 @@
             }
         }
 
-        function edit(ontologyIndex, classIndex, propertyIndex) {
-            _saveState(ontologyIndex, classIndex, propertyIndex);
-            _setState();
-        }
-
-        function submitOntology(isValid) {
-        }
-
-        function createClass(ontology) {
-        }
-
-        function editClass(ontology, classObj) {
-        }
-
-        function submitClass(isValid) {
-        }
-
-        function createProperty(ontology, classObj) {
-        }
-
-        function editProperty(ontology, classObj, property) {
-        }
-
-        function submitProperty(isValid) {
+        // sets the state to edit the selected object and saves the previous state that they just left
+        function edit(oi, ci, pi) {
+            _saveState(oi, ci, pi);
+            _setState(oi, ci, pi);
         }
     }
 })();
