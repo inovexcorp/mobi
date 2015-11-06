55449326238573f7a712c4a9fa5bac2502080581
Bryan Miller <bryan@Bryans-MacBook-Pro.local>
2015-11-06 10:08:22 -0500

    initial camel-rest

--- etl/etl.rest/src/main/java/org/matonto/etl/rest/Rest.java
+++ etl/etl.rest/src/main/java/org/matonto/etl/rest/Rest.java
@@ -1,7 +1,18 @@
 package org.matonto.etl.rest;
 
+import io.swagger.annotations.ApiModelProperty;
+import org.openrdf.query.algebra.Str;
+import org.openrdf.query.algebra.evaluation.function.rdfterm.UUID;
+import io.swagger.annotations.ApiModel;
+
 /**
  * Created by bryan on 11/2/15.
  */
+@ApiModel(description = "Represents an user of the system")
 public class Rest {
+
+    @ApiModelProperty(value = "The name of the user", required = true)
+    public String generateUUID(){
+        return java.util.UUID.randomUUID().toString();
+    }
 }
