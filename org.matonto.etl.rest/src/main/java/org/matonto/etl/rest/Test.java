3e109f46cbc509082b996df3b93b5835005b134f
Bryan Miller <bryan@Bryans-MacBook-Pro.local>
2015-11-12 11:07:29 -0500

    Working swagger/rest test

---
+++ org.matonto.etl.rest/src/main/java/org/matonto/etl/rest/Test.java
@@ -0,0 +1,21 @@
+package org.matonto.etl.rest;
+
+import io.swagger.annotations.ApiModel;
+
+import java.util.UUID;
+
+/**
+ * Created by bryan on 11/12/15.
+ */
+public class Test {
+
+    /**
+     * Gets a random string
+     *
+     * @return a uuid
+     */
+    public String generateUUID(){
+        return UUID.randomUUID().toString();
+    }
+
+}
---
+++ org.matonto.etl.rest/src/main/java/org/matonto/etl/rest/User.java
@@ -0,0 +1,37 @@
+package org.matonto.etl.rest;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Represents an user of the system")
+public class User {
+
+    private int id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @ApiModelProperty(value = "RAWR RAWR RAWR", required = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ApiModelProperty(value = "The name of the user", required = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
