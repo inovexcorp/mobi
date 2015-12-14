b8474fe9c92e936e6a80e2ddb615a96e6b4c30c2
Bryan Miller <bryan.miller@inovexcorp.com>
2015-12-14 14:43:35 -0500

    Add CLITransform Class

---
+++ org.matonto.etl.cli/src/main/java/org/matonto/etl/cli/CLITransform.java
@@ -0,0 +1,8 @@
+package org.matonto.etl.cli;
+
+import org.apache.karaf.shell.api.action.Command;
+
+@Command(scope = "matonto", name = "transform", description = "Transforms CSV Files to RDF using a mapping file")
+public class CLITransform {
+
+}
