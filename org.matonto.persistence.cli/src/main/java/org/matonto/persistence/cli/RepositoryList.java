bc3d6a4be0954190542eef5ef0741156fee6881a
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2015-12-14 22:46:35 -0500

    Fixed dependency injection for cli

--- org.matonto.persistence.cli/src/main/java/org/matonto/persistence/cli/RepositoryList.java
+++ org.matonto.persistence.cli/src/main/java/org/matonto/persistence/cli/RepositoryList.java
@@ -1,24 +1,26 @@
 package org.matonto.persistence.cli;
 
-import aQute.bnd.annotation.component.Reference;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.support.table.ShellTable;
 import org.matonto.repository.api.RepositoryManager;
 import org.matonto.repository.config.RepositoryConfig;
 
-import java.util.Set;
-
 @Command(scope = "matonto", name = "repository-list", description = "Lists the currently managed repositories.")
 @Service
 public class RepositoryList implements Action {
 
+    @Reference
     private RepositoryManager repositoryManager;
 
-    @Reference
     public void setRepositoryManager(RepositoryManager repositoryManager) {
         this.repositoryManager = repositoryManager;
+    }
+
+    public void unsetRepositoryManager(RepositoryManager repositoryManager) {
+        this.repositoryManager = null;
     }
 
     @Override
@@ -29,12 +31,12 @@ public class RepositoryList implements Action {
         table.column("Title");
         table.emptyTableText("No Repositories");
 
-        Set<String> repos = repositoryManager.getAllRepositories().keySet();
-
-        repositoryManager.getAllRepositories().forEach((repoID, repo) -> {
-            RepositoryConfig config = repo.getConfig();
-            table.addRow().addContent(config.id(), config.title());
-        });
+        if (repositoryManager != null) {
+            repositoryManager.getAllRepositories().forEach((repoID, repo) -> {
+                RepositoryConfig config = repo.getConfig();
+                table.addRow().addContent(config.id(), config.title());
+            });
+        }
 
         table.print(System.out);
         return null;
