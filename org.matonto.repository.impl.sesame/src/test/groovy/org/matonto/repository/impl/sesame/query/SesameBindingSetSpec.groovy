e32f04f37f3550798fb1a62c1f6a4b830b6bc8a3
Bryan Miller <bryan.miller@inovexcorp.com>
2016-01-22 11:07:14 -0500

    Add more tests to query api

--- org.matonto.repository.impl.sesame/src/test/groovy/org/matonto/repository/impl/sesame/query/SesameBindingSetSpec.groovy
+++ org.matonto.repository.impl.sesame/src/test/groovy/org/matonto/repository/impl/sesame/query/SesameBindingSetSpec.groovy
@@ -1,7 +1,42 @@
 package org.matonto.repository.impl.sesame.query
 
+import org.openrdf.query.BindingSet
 import spock.lang.Specification
 
 class SesameBindingSetSpec extends Specification{
+
+    def "hasBindingSet(String) returns true when binding set exists"() {
+
+        setup:
+        BindingSet sesBindingSet = Mock()
+        SesameBindingSet bindingSet = new SesameBindingSet(sesBindingSet)
+
+        then:
+        bindingSet.hasBinding("test");
+        1 * sesBindingSet.hasBinding("test") >> true
+
+    }
+
+    def "hasBindingSet(String) returns false when binding set doesn't exists"() {
+        setup:
+        BindingSet sesBindingSet = Mock()
+        SesameBindingSet bindingSet = new SesameBindingSet(sesBindingSet)
+
+        then:
+        !bindingSet.hasBinding("test");
+        1 * sesBindingSet.hasBinding("test") >> false
+    }
+
+    def "getBindingNames() returns all binding names" () {
+        setup:
+        BindingSet sesBindingSet = Mock()
+        SesameBindingSet bindingSet = new SesameBindingSet(sesBindingSet)
+        Set<String> bindingNames = ["test"]
+
+        then:
+        bindingSet.getBindingNames().equals(bindingNames)
+        1 * sesBindingSet >> bindingNames
+    }
+
 
 }
--- org.matonto.repository.impl.sesame/src/test/groovy/org/matonto/repository/impl/sesame/query/SesameGraphQueryResultSpec.groovy
+++ org.matonto.repository.impl.sesame/src/test/groovy/org/matonto/repository/impl/sesame/query/SesameGraphQueryResultSpec.groovy
@@ -1,7 +1,46 @@
 package org.matonto.repository.impl.sesame.query
 
+import org.matonto.rdf.api.Statement
+import org.matonto.rdf.api.ValueFactory
+import org.matonto.rdf.core.impl.sesame.SimpleValueFactory
+import org.matonto.rdf.core.utils.Values
 import spock.lang.Specification
 
 class SesameGraphQueryResultSpec extends Specification {
+
+    def "getNamespaces() returns all namespaces"(){
+        org.openrdf.query.GraphQueryResult sesGQR = Mock()
+        SesameGraphQueryResult graphQueryResult = new SesameGraphQueryResult(sesGQR)
+        Map<String,String> nameSpaces = [Prefix:"Namespace"]
+
+        then:
+        graphQueryResult.getNamespaces().equals(nameSpaces)
+        1 * sesGQR.getNamespaces() >> nameSpaces
+    }
+
+    def "hasNext() returns true if sesGQR has statments"() {
+        org.openrdf.query.GraphQueryResult sesGQR = Mock()
+        SesameGraphQueryResult graphQueryResult = new SesameGraphQueryResult(sesGQR)
+
+        then:
+        graphQueryResult.hasNext()
+        1 * sesGQR.hasNext() >> true
+    }
+
+    def "next() returns next statment in result"() {
+        org.openrdf.query.GraphQueryResult sesGQR = Mock()
+        SesameGraphQueryResult graphQueryResult = new SesameGraphQueryResult(sesGQR)
+        ValueFactory vf = SimpleValueFactory.getInstance();
+        org.openrdf.model.IRI subj = vf.createIRI("http://test.com/sub")
+        org.openrdf.model.IRI pred = vf.createIRI("http://test.com/pred")
+        org.openrdf.model.IRI obj = vf.createIRI("http://test.com/obj")
+        org.openrdf.model.Statement sesStatement = vf.createStatement(subj, pred, obj)
+        Statement s = Values.matontoStatement(sesStatement)
+
+        then:
+        s = graphQueryResult.next()
+        1 * sesGQR.next() >> sesStatement
+
+    }
 
 }
