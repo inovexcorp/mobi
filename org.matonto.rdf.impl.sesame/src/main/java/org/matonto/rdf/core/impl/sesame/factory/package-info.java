3d533c50b0c6a124addb2dc60f97c9892abe1719
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2016-01-27 13:44:21 -0500

    Added package-info.java for persistence bundles

---
+++ org.matonto.rdf.impl.sesame/src/main/java/org/matonto/rdf/core/impl/sesame/factory/package-info.java
@@ -0,0 +1,5 @@
+@Version("1.0.1.${build}")
+
+package org.matonto.rdf.core.impl.sesame.factory;
+
+import aQute.bnd.annotation.Version;
---
+++ org.matonto.rdf.impl.sesame/src/main/java/org/matonto/rdf/core/utils/package-info.java
@@ -0,0 +1,5 @@
+@Version("1.0.1.${build}")
+
+package org.matonto.rdf.core.utils;
+
+import aQute.bnd.annotation.Version;
