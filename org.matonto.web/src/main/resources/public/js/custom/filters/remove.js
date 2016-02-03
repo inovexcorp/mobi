6b83d212f5c6e3e157f8c7e9d3066f168640b5f2
Jordan Lewis <jordan.lewis@inovexcorp.com>
2016-02-03 09:43:11 -0500

    Adding array support for remove and removing the default values for the drop ui-selects.

--- org.matonto.web/src/main/resources/public/js/custom/filters/remove.js
+++ org.matonto.web/src/main/resources/public/js/custom/filters/remove.js
@@ -6,18 +6,32 @@
         .filter('remove', remove);
 
     function remove() {
-        return function(arr, string) {
-            var temp = [],
+        return function(arr, item) {
+            var temp, j, found,
+                result = [],
                 i = 0;
 
             while(i < arr.length) {
-                if(string !== (arr[i].namespace + arr[i].localName)) {
-                    temp.push(arr[i]);
+                temp = arr[i].namespace + arr[i].localName;
+                if(Array.isArray(item)) {
+                    j = 0;
+                    found = false;
+                    while(j < item.length) {
+                        if(temp === item[j]['@id']) {
+                            found = true;
+                            break;
+                        }
+                        j++;
+                    }
+                    if(!found) {
+                        result.push(arr[i]);
+                    }
+                } else if(item !== temp) {
+                    result.push(arr[i]);
                 }
                 i++;
             }
-
-            return temp;
+            return result;
         }
     }
 })();
