8d1464759f3fe45c0dabbc6d8d2599e6a9b410f8
Jordan Lewis <jordan.lewis@inovexcorp.com>
2016-02-10 09:54:43 -0500

    Adding array check.

--- org.matonto.web/src/main/resources/public/js/custom/filters/removeIriFromArray.js
+++ org.matonto.web/src/main/resources/public/js/custom/filters/removeIriFromArray.js
@@ -7,14 +7,17 @@
 
     function removeIriFromArray() {
         return function(arr, string) {
-            var temp = [],
-                i = 0;
+            var temp = [];
 
-            while(i < arr.length) {
-                if(string !== (arr[i].namespace + arr[i].localName)) {
-                    temp.push(arr[i]);
+            if(Array.isArray(arr)) {
+                var i = 0;
+
+                while(i < arr.length) {
+                    if(string !== (arr[i].namespace + arr[i].localName)) {
+                        temp.push(arr[i]);
+                    }
+                    i++;
                 }
-                i++;
             }
 
             return temp;
