42c11a90ed39dc85e7ba459c8358fece0ed42de6
Jordan Lewis <jordan.lewis@inovexcorp.com>
2016-02-18 13:29:09 -0500

    Adding new tree item with sub lists directive.

---
+++ org.matonto.web/src/main/resources/public/modules/ontology-editor/directives/treeItemWithSub/treeItemWithSub.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('treeItemWithSub', [])
+        .directive('treeItemWithSub', treeItemWithSub);
+
+        function treeItemWithSub() {
+            return {
+                restrict: 'E',
+                transclude: true,
+                remove: true,
+                scope: {
+                    currentEntity: '=',
+                    currentOntology: '=',
+                    isActive: '=',
+                    isOpened: '=',
+                    onClick: '&'
+                },
+                templateUrl: 'modules/ontology-editor/directives/treeItemWithSub/treeItemWithSub.html'
+            }
+        }
+})();
