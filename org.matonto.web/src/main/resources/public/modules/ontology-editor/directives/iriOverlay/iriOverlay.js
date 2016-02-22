e76bbf6138aad25725c98b2ea4f383f252e05f37
Jordan Lewis <jordan.lewis@inovexcorp.com>
2016-02-22 14:01:19 -0500

    Updating the iri overlay code to assign variables inside the controller instead of with ng-init.

--- org.matonto.web/src/main/resources/public/modules/ontology-editor/directives/iriOverlay/iriOverlay.js
+++ org.matonto.web/src/main/resources/public/modules/ontology-editor/directives/iriOverlay/iriOverlay.js
@@ -8,7 +8,16 @@
         function iriOverlay() {
             return {
                 restrict: 'E',
-                templateUrl: 'modules/ontology-editor/directives/iriOverlay/iriOverlay.html'
+                templateUrl: 'modules/ontology-editor/directives/iriOverlay/iriOverlay.html',
+                controller: ['$scope', '$filter', function($scope, $filter) {
+                    var vm = $scope.$parent.vm,
+                        iri = vm.selected.matonto.namespace ? (vm.selected.matonto.namespace + vm.selected['@id']) : $filter('splitIRI')(vm.selected['@id']);
+
+                    vm.iriBegin = iri.begin;
+                    vm.iriThen = iri.then;
+                    vm.iriEnd = iri.end || $filter('camelCase')(vm.selected[vm.rdfs + 'label'][0]['@value']);
+                    vm.iriUpdate = true;
+                }]
             }
         }
 })();
