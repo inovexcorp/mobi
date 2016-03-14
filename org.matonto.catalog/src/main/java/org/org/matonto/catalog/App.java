eef463171743ca8c02242aadd96811b3e99d8cdb
Joy Hedges <joy@matonto.org>
2016-03-14 11:27:44 -0400

    created catalog project module

---
+++ org.matonto.catalog/src/main/java/org/org/matonto/catalog/App.java
@@ -0,0 +1,13 @@
+package org.org.matonto.catalog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
