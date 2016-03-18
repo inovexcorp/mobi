aa09d2caf8a6914385c4ec2dbd09c10da6db1d16
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2016-03-18 15:17:44 -0400

    Added Simple Catalog and test

---
+++ org.matonto.catalog.api/src/main/java/org/matonto/catalog/impl/SimpleCatalog.java
@@ -0,0 +1,43 @@
+package org.matonto.catalog.impl;
+
+import org.matonto.catalog.api.Catalog;
+
+import java.time.OffsetDateTime;
+
+public class SimpleCatalog implements Catalog {
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public OffsetDateTime getIssued() {
+        return null;
+    }
+
+    @Override
+    public OffsetDateTime getModified() {
+        return null;
+    }
+
+    @Override
+    public String getLanguage() {
+        return null;
+    }
+
+    @Override
+    public String getLicense() {
+        return null;
+    }
+
+    @Override
+    public String getRights() {
+        return null;
+    }
+}
