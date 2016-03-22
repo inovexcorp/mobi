4d8abedbc01ec1ba84a33686b84fe85be30ddb9d
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2016-03-22 10:30:37 -0400

    Implemented Catalog and tests

--- org.matonto.catalog.api/src/main/java/org/matonto/catalog/api/Catalog.java
+++ org.matonto.catalog.api/src/main/java/org/matonto/catalog/api/Catalog.java
@@ -4,17 +4,45 @@ import java.time.OffsetDateTime;
 
 public interface Catalog {
 
+    /**
+     * Returns the title of the Catalog.
+     *
+     * @return The String representing the Catalog title.
+     */
     String getTitle();
 
+    /**
+     * Returns the Catalog description.
+     *
+     * @return The String representing the Catalog description.
+     */
     String getDescription();
 
+    /**
+     * Returns the Catalog issue datetime.
+     *
+     * @return The OffsetDateTime representing the Catalog issue datetime.
+     */
     OffsetDateTime getIssued();
 
+    /**
+     * Returns the Catalog last modified datetime.
+     *
+     * @return The OffsetDateTime representing the Catalog last modified datetime.
+     */
     OffsetDateTime getModified();
 
-    String getLanguage();
-
+    /**
+     * Returns the Catalog license.
+     *
+     * @return The String representing the Catalog license.
+     */
     String getLicense();
 
+    /**
+     * Returns the Catalog rights.
+     *
+     * @return The String representing the Catalog rights.
+     */
     String getRights();
 }
--- org.matonto.catalog.impl/src/main/java/org/matonto/catalog/impl/SimpleCatalog.java
+++ org.matonto.catalog.impl/src/main/java/org/matonto/catalog/impl/SimpleCatalog.java
@@ -4,40 +4,106 @@ import org.matonto.catalog.api.Catalog;
 
 import java.time.OffsetDateTime;
 
-public class SimpleCatalog implements Catalog {
+class SimpleCatalog implements Catalog {
+
+    private final String title;
+    private final String description;
+    private final OffsetDateTime issued;
+    private final OffsetDateTime modified;
+    private final String license;
+    private final String rights;
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return description;
     }
 
     @Override
     public OffsetDateTime getIssued() {
-        return null;
+        return issued;
     }
 
     @Override
     public OffsetDateTime getModified() {
-        return null;
-    }
-
-    @Override
-    public String getLanguage() {
-        return null;
+        return modified;
     }
 
     @Override
     public String getLicense() {
-        return null;
+        return license;
     }
 
     @Override
     public String getRights() {
-        return null;
+        return rights;
+    }
+
+    public static class Builder {
+        private final String title;
+
+        private String description = "";
+        private OffsetDateTime issued = OffsetDateTime.now();
+        private OffsetDateTime modified = null;
+        private String license = "";
+        private String rights = "";
+
+        /**
+         * Builder for SimpleCatalog. Title is required. Issued and Modified Dates default to time
+         * of Builder creation. All other parameters are optional and default to empty strings.
+         *
+         * @param title The Catalog title.
+         */
+        public Builder(String title) {
+            this.title = title;
+        }
+
+        public Builder description(String val) {
+            this.description = val;
+            return this;
+        }
+
+        public Builder issued(OffsetDateTime val) {
+            this.issued = val;
+            return this;
+        }
+
+        public Builder modified(OffsetDateTime val) {
+            this.modified = val;
+            return this;
+        }
+
+        public Builder license(String val) {
+            this.license = val;
+            return this;
+        }
+
+        public Builder rights(String val) {
+            this.rights = val;
+            return this;
+        }
+
+        public SimpleCatalog build() {
+            if (modified == null) {
+                modified = issued;
+            } else if (issued.isAfter(modified)) {
+                throw new IllegalStateException("Modified time must occur after issued time.");
+            }
+
+            return new SimpleCatalog(this);
+        }
+    }
+
+    private SimpleCatalog(Builder builder) {
+        title = builder.title;
+        description = builder.description;
+        issued = builder.issued;
+        modified = builder.modified;
+        license = builder.license;
+        rights = builder.rights;
     }
 }
