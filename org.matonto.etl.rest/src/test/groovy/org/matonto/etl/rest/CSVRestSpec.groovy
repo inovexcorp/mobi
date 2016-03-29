e0ab213323b16ba034fad309798ca42e4c5d4811
Megan Rodriguez <megan.rodriguez@inovexcorp.com>
2016-03-29 09:53:12 -0400

    Create initial directory structure and Spock class for testing.

---
+++ org.matonto.etl.rest/src/test/groovy/org/matonto/etl/rest/CSVRestSpec.groovy
@@ -0,0 +1,9 @@
+package org.matonto.etl.rest
+
+import org.matonto.etl.rest.impl.CSVRestImpl;
+import spock.lang.Specification
+
+class CSVRestSpec extends Specification {
+    def restimpl = new CSVRestImpl();
+
+}
