e8bc60f0db986a106e2210b051408e10857d3dfb
Megan Rodriguez <megan.rodriguez@inovexcorp.com>
2016-04-15 15:22:20 -0400

    Create directive for inputting a mapping name. Add overlay for editing maping name when editing.

---
+++ org.matonto.web/src/test/js/MappingNameOverlaySpec.js
@@ -0,0 +1,70 @@
+describe('Mapping Name Overlay directive', function() {
+    var $compile,
+        scope,
+        mappingManagerSvc;
+
+    mockMappingManager();
+    beforeEach(function() {
+        module('mappingNameOverlay');
+
+        inject(function(mappingManagerService) {
+            mappingManagerSvc = mappingManagerService;
+        });
+
+        inject(function(_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            scope = _$rootScope_;
+        });
+    });
+
+    injectDirectiveTemplate('modules/mapper/directives/mappingNameOverlay/mappingNameOverlay.html');
+
+    describe('in isolated scope', function() {
+        beforeEach(function() {
+            scope.mappingName = '';
+            scope.set = jasmine.createSpy('set');
+            scope.close = jasmine.createSpy('close');
+
+            this.element = $compile(angular.element('<mapping-name-overlay mapping-name="{{mappingName}}" set="set(name)" close="close()"></mapping-name-overlay>'))(scope);
+            scope.$digest();
+        });
+
+        it('mappingName should be one way bound', function() {
+            var isolatedScope = this.element.isolateScope();
+            isolatedScope.mappingName = 'test';
+            scope.$digest();
+            expect(scope.mappingName).not.toBe('test');
+        });
+        it('set should be called in the parent scope', function() {
+            var isolatedScope = this.element.isolateScope();
+            isolatedScope.set();
+
+            expect(scope.set).toHaveBeenCalled();
+        });
+        it('close should be called in the parent scope', function() {
+            var isolatedScope = this.element.isolateScope();
+            isolatedScope.close();
+
+            expect(scope.close).toHaveBeenCalled();
+        });
+    });
+    describe('replaces the element with the correct html', function() {
+        beforeEach(function() {
+            this.element = $compile(angular.element('<mapping-name-overlay mapping-name="{{mappingName}}" set="set(name)" close="close()"></mapping-name-overlay>'))(scope);
+            scope.$digest();
+        });
+        it('for wrapping containers', function() {
+            expect(this.element.hasClass('mapping-name-overlay')).toBe(true);
+            expect(this.element.querySelectorAll('form.content').length).toBe(1);
+        });
+        it('with a mapping name input', function() {
+            expect(this.element.find('mapping-name-input').length).toBe(1);
+        });
+        it('with custom buttons for cancel and set', function() {
+            var buttons = this.element.find('custom-button');
+            expect(buttons.length).toBe(2);
+            expect(['Cancel', 'Set'].indexOf(angular.element(buttons[0]).text()) >= 0).toBe(true);
+            expect(['Cancel', 'Set'].indexOf(angular.element(buttons[1]).text()) >= 0).toBe(true);
+        });
+    });
+});
