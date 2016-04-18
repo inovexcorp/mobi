303a1ea3ca2bf84144aa75b8161ec083c874cf17
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2016-04-18 19:31:22 -0400

    Documented Catalog REST Interface

--- org.matonto.catalog.rest/src/main/java/org/matonto/catalog/rest/impl/CatalogRestImpl.java
+++ org.matonto.catalog.rest/src/main/java/org/matonto/catalog/rest/impl/CatalogRestImpl.java
@@ -3,13 +3,17 @@ package org.matonto.catalog.rest.impl;
 import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Reference;
 import org.matonto.catalog.api.CatalogManager;
+import org.matonto.catalog.api.Distribution;
 import org.matonto.catalog.api.PublishedResource;
 import org.matonto.catalog.rest.CatalogRest;
+import org.matonto.catalog.rest.jaxb.DistributionMarshaller;
+import org.matonto.catalog.rest.jaxb.PaginatedResults;
 import org.matonto.catalog.rest.jaxb.PublishedResourceMarshaller;
 import org.matonto.rdf.api.Resource;
 import org.matonto.rdf.api.ValueFactory;
 import org.matonto.rest.util.ErrorUtils;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.core.Response;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
@@ -57,6 +61,46 @@ public class CatalogRestImpl implements CatalogRest {
         } else {
             return null;
         }
+    }
+
+    @Override
+    public Response createPublishedResource(PublishedResourceMarshaller resource, @DefaultValue("http://matonto.org/ontologies/catalog#PublishedResource") String resourceType) {
+        return null;
+    }
+
+    @Override
+    public Response deletePublishedResource(String resourceId) {
+        return null;
+    }
+
+    @Override
+    public PaginatedResults<PublishedResourceMarshaller> listPublishedResources(@DefaultValue("http://matonto.org/ontologies/catalog#PublishedResource") String resourceType, String searchTerms, @DefaultValue("100") int limit, @DefaultValue("0") int start) {
+        return null;
+    }
+
+    @Override
+    public PaginatedResults<DistributionMarshaller> getDistributions(String resourceId, @DefaultValue("100") int limit, @DefaultValue("0") int start) {
+        return null;
+    }
+
+    @Override
+    public Response createDistribution(Distribution distribution, String resourceId) {
+        return null;
+    }
+
+    @Override
+    public Response deleteDistributions(String resourceId) {
+        return null;
+    }
+
+    @Override
+    public Response deleteDistribution(String resourceId, String distributionId) {
+        return null;
+    }
+
+    @Override
+    public Response getPublishedResource() {
+        return null;
     }
 
     private boolean isBNodeString(String string) {
---
+++ org.matonto.catalog.rest/src/main/java/org/matonto/catalog/rest/jaxb/DistributionMarshaller.java
@@ -0,0 +1,7 @@
+package org.matonto.catalog.rest.jaxb;
+
+/**
+ * Created by stephen on 4/18/16.
+ */
+public class DistributionMarshaller {
+}
