81f9c5c5fc3fb56768e6102c18143e64294a93b0
Jordan Lewis <jordan.lewis@inovexcorp.com>
2016-05-24 15:02:05 -0400

    Adding tab button container directive and fixing rounding errors with table display.

--- org.matonto.web/src/main/resources/public/modules/ontology-editor/directives/defaultTab/defaultTab.js
+++ org.matonto.web/src/main/resources/public/modules/ontology-editor/directives/defaultTab/defaultTab.js
@@ -9,25 +9,7 @@
             return {
                 restrict: 'E',
                 replace: true,
-                templateUrl: 'modules/ontology-editor/directives/defaultTab/defaultTab.html',
-                controllerAs: 'dvm',
-                controller: function() {
-                    var dvm = this;
-
-                    dvm.featureList = [
-                        'Upload and download turtle, RDF/XML, and OWL/XML ontology serializations.',
-                        'View the ontology in four different tree views which show everything, classes only, object properties only, and datatype properties only.',
-                        'Create a new ontology which is persisted in the repository and can be downloaded in supported serializations.',
-                        'Add classes and/or properties to uploaded ontologies.',
-                        'Add common or custom annotations to your ontologies, classes, and properties.',
-                        'Delete ontologies from your repository.',
-                        'Open and close ontologies to show or hide them from your view without deletion.',
-                        'Select axiom values from all imported ontologies.',
-                        'Set axioms on classes and/or properties with a searchable list of options sorted by ontology IRI.',
-                        'Preview the current changes to your ontology in supported serializations.',
-                        'View blank node values in an easy to read format for the following: restrictions, unions, and intersections.'
-                    ]
-                }
+                templateUrl: 'modules/ontology-editor/directives/defaultTab/defaultTab.html'
             }
         }
 })();
