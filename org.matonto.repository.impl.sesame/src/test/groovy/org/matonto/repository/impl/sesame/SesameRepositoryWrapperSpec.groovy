83d421b719af71ec1fe747a15c7c5e7f2bf175cc
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2016-06-13 14:44:39 -0400

    Added license header to files

--- org.matonto.repository.impl.sesame/src/test/groovy/org/matonto/repository/impl/sesame/SesameRepositoryWrapperSpec.groovy
+++ org.matonto.repository.impl.sesame/src/test/groovy/org/matonto/repository/impl/sesame/SesameRepositoryWrapperSpec.groovy
@@ -12,6 +12,29 @@ class SesameRepositoryWrapperSpec extends Specification {
     @Rule
     public TemporaryFolder tempDir = new TemporaryFolder();
 
+/*-
+ * #%L
+ * org.matonto.repository.impl.sesame
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2016 iNovex Information Systems, Inc.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
     def memRepo = new SesameRepositoryWrapper(new SailRepository(new MemoryStore()))
     def persistedMemRepo
 
