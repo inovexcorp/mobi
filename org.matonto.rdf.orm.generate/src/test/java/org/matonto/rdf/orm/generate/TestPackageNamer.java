f57cbd0dc73e836847cfb08d9a9a5540f2734fde
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2016-08-18 16:59:04 -0400

    Updated pom versions, added license headers, used new junit packages

--- org.matonto.rdf.orm.generate/src/test/java/org/matonto/rdf/orm/generate/TestPackageNamer.java
+++ org.matonto.rdf.orm.generate/src/test/java/org/matonto/rdf/orm/generate/TestPackageNamer.java
@@ -2,23 +2,19 @@ package org.matonto.rdf.orm.generate;
 
 import org.junit.Test;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
 public class TestPackageNamer {
 
-	public static final String extendedDocumentMetadata = "http://cambridgesemantics.com/ontologies/2012/07/ExtendedDocumentMetadata";
-	public static final String lillyCore = "http://www.lilly.com/ontologies/2015/SDP/LillyCore";
-	public static final String lillyService = "http://www.lilly.com/ontologies/2015/SDP/LillyServices";
-	public static final String lillySn6 = "http://www.lilly.com/ontologies/2015/SDP/LillyCore/SN6";
+    private static final String extendedDocumentMetadata = "http://cambridgesemantics.com/ontologies/2012/07/ExtendedDocumentMetadata";
+    private static final String lillyCore = "http://www.lilly.com/ontologies/2015/SDP/LillyCore";
+    private static final String lillyService = "http://www.lilly.com/ontologies/2015/SDP/LillyServices";
 
 	@Test
 	public void testGood() throws Exception {
-		TestCase.assertEquals("com.cambridgesemantics.ontologies._2012._07.extendeddocumentmetadata",
+		assertEquals("com.cambridgesemantics.ontologies._2012._07.extendeddocumentmetadata",
 				PackageNamer.packageFromUrl(extendedDocumentMetadata));
-		TestCase.assertEquals("com.lilly.www.ontologies._2015.sdp.lillycore", PackageNamer.packageFromUrl(lillyCore));
-		TestCase.assertEquals("com.lilly.www.ontologies._2015.sdp.lillyservices",
-				PackageNamer.packageFromUrl(lillyService));
-
+		assertEquals("com.lilly.www.ontologies._2015.sdp.lillycore", PackageNamer.packageFromUrl(lillyCore));
+		assertEquals("com.lilly.www.ontologies._2015.sdp.lillyservices", PackageNamer.packageFromUrl(lillyService));
 	}
-
 }
--- org.matonto.rdf.orm.generate/src/test/java/org/matonto/rdf/orm/generate/TestSourceGenerator.java
+++ org.matonto.rdf.orm.generate/src/test/java/org/matonto/rdf/orm/generate/TestSourceGenerator.java
@@ -28,7 +28,7 @@ import org.matonto.rdf.orm.impl.ThingFactory;
 import com.xmlns.foaf._0._1.Agent;
 import com.xmlns.foaf._0._1.AgentFactory;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class TestSourceGenerator {
@@ -65,7 +65,7 @@ public class TestSourceGenerator {
 					"http://xmlns.com/foaf/0.1/"), "http://xmlns.com/foaf/0.1/", "target/generated-test-sources");
 		} catch (Exception e) {
 			e.printStackTrace();
-			TestCase.fail(e.getMessage());
+			fail(e.getMessage());
 		}
 
 	}
@@ -105,16 +105,16 @@ public class TestSourceGenerator {
 		final AgentFactory factory = new AgentFactory();
 		final Agent a = factory.getExisting(valueFactory.createIRI("urn://matonto.org/orm/test/testAgent"), model,
 				valueFactory, valueConverterRegistry);
-		TestCase.assertEquals(valueFactory.createLiteral(100), a.getAge());
-		TestCase.assertEquals(valueFactory.createLiteral("male"), a.getGender());
+		assertEquals(valueFactory.createLiteral(100), a.getAge());
+		assertEquals(valueFactory.createLiteral("male"), a.getGender());
 		final Set<Thing> mboxes = a.getMbox();
-		TestCase.assertNotNull(mboxes);
-		TestCase.assertFalse(mboxes.isEmpty());
+		assertNotNull(mboxes);
+		assertFalse(mboxes.isEmpty());
 		final Thing mbox = mboxes.iterator().next();
-		TestCase.assertEquals(valueFactory.createLiteral("tester@gmail.com"),
+		assertEquals(valueFactory.createLiteral("tester@gmail.com"),
 				mbox.getProperty(valueFactory.createIRI("http://xmlns.com/foaf/0.1/accountName"),
 						valueFactory.createIRI("urn://matonto.org/orm/test/account")));
-		TestCase.assertEquals(valueFactory.createIRI("urn://matonto.org/orm/test/account"), mbox.getResource());
+		assertEquals(valueFactory.createIRI("urn://matonto.org/orm/test/account"), mbox.getResource());
 	}
 
 }
