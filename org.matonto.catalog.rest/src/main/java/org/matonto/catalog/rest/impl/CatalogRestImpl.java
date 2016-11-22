a212a3aafb4a89d02f090c164feab3a531f1c581
Megan Mercer <megan.mercer@inovexcorp.com>
2016-11-22 17:14:09 -0500

    Updates to licenses. Change CatalogRestImpl to define new methods.

--- org.matonto.catalog.rest/src/main/java/org/matonto/catalog/rest/impl/CatalogRestImpl.java
+++ org.matonto.catalog.rest/src/main/java/org/matonto/catalog/rest/impl/CatalogRestImpl.java
@@ -25,33 +25,21 @@ package org.matonto.catalog.rest.impl;
 
 import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Reference;
-import net.sf.json.JSONArray;
 import org.apache.log4j.Logger;
-import org.matonto.catalog.api.*;
+import org.matonto.catalog.api.CatalogManager;
+import org.matonto.catalog.api.ontologies.mcat.*;
 import org.matonto.catalog.rest.CatalogRest;
-import org.matonto.catalog.rest.jaxb.DistributionMarshaller;
-import org.matonto.catalog.rest.jaxb.PublishedResourceMarshaller;
-import org.matonto.persistence.utils.Values;
-import org.matonto.rdf.api.IRI;
-import org.matonto.rdf.api.Resource;
 import org.matonto.rdf.api.ValueFactory;
-import org.matonto.rest.util.ErrorUtils;
-import org.matonto.rest.util.LinksUtils;
-import org.matonto.rest.util.jaxb.PaginatedResults;
 
-import java.time.OffsetDateTime;
 import java.util.*;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
 
 @Component(immediate = true)
 public class CatalogRestImpl implements CatalogRest {
 
     private CatalogManager catalogManager;
-    private Values values;
     private ValueFactory valueFactory;
     private CatalogFactory catalogFactory;
 
@@ -82,11 +70,6 @@ public class CatalogRestImpl implements CatalogRest {
     }
 
     @Reference
-    protected void setValues(Values values) {
-        this.values = values;
-    }
-
-    @Reference
     protected void setValueFactory(ValueFactory valueFactory) {
         this.valueFactory = valueFactory;
     }
@@ -97,209 +80,226 @@ public class CatalogRestImpl implements CatalogRest {
     }
 
     @Override
-    public PublishedResourceMarshaller getPublishedResource(String resourceId) {
-        if (resourceId == null) {
-            throw ErrorUtils.sendError("Must provide a resource ID.", Response.Status.BAD_REQUEST);
-        }
-
-        Optional<PublishedResource> publishedResourceOptional =
-                catalogManager.getResource(values.getIriOrBnode(resourceId));
-
-        if (publishedResourceOptional.isPresent()) {
-            return processResource(publishedResourceOptional.get());
-        } else {
-            return null;
-        }
-    }
-
-    @Override
-    public Response createPublishedResource(PublishedResourceMarshaller resource, String resourceType) {
+    public Response getCatalogs(String catalogType) {
         return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
-    public Response deletePublishedResource(String resourceId) {
+    public Response getCatalog(String catalogId) {
         return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
-    public PaginatedResults<PublishedResourceMarshaller> listPublishedResources(UriInfo uriInfo, String resourceType,
-                                                                                String searchTerms, String sortBy,
-                                                                                boolean asc, int limit, int start) {
-        IRI sortResource = valueFactory.createIRI(sortBy);
-
-        PaginatedSearchParamsBuilder builder = catalogFactory.createSearchParamsBuilder(limit, start, sortResource)
-                .ascending(asc);
-
-        if (resourceType != null) {
-            builder.typeFilter(valueFactory.createIRI(resourceType));
-        }
-
-        PaginatedSearchParams searchParams = builder.build();
-
-        PaginatedSearchResults<PublishedResource> searchResults = catalogManager.findResource(searchParams);
-
-        List<PublishedResourceMarshaller> publishedResources = new ArrayList<>();
-        searchResults.getPage().forEach(resource -> publishedResources.add(processResource(resource)));
-
-        int size = searchResults.getPage().size();
-
-        PaginatedResults<PublishedResourceMarshaller> marshaller = new PaginatedResults<>();
-        marshaller.setResults(publishedResources);
-        marshaller.setLimit(limit);
-        marshaller.setSize(size);
-        marshaller.setStart(start);
-        marshaller.setTotalSize(searchResults.getTotalSize());
-
-        marshaller.setLinks(LinksUtils.buildLinks(uriInfo, size, searchResults.getTotalSize(), limit, start));
-
-        return marshaller;
-    }
-
-    @Override
-    public Set<DistributionMarshaller> getDistributions(String resourceId) {
-        if (resourceId == null) {
-            throw ErrorUtils.sendError("Must provide a resource ID.", Response.Status.BAD_REQUEST);
-        }
-
-        Optional<PublishedResource> publishedResourceOptional =
-                catalogManager.getResource(values.getIriOrBnode(resourceId));
-
-        if (publishedResourceOptional.isPresent()) {
-            PublishedResource publishedResource = publishedResourceOptional.get();
-
-            Set<DistributionMarshaller> distributions = new HashSet<>();
-            publishedResource.getDistributions().forEach(distribution ->
-                    distributions.add(processDistribution(distribution)));
-
-            return distributions;
-        } else {
-            return null;
-        }
-    }
-
-    @Override
-    public Response createDistribution(Distribution distribution, String resourceId) {
+    public Response getRecords(String catalogId, String sort, String recordType, int offset, int limit, String searchText) {
         return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
-    public Response deleteDistributions(String resourceId) {
+    public <T extends Record> Response createRecord(String catalogId, T newRecord) {
         return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
-    public DistributionMarshaller getDistribution(String resourceId, String distributionId) {
-        if (resourceId == null || distributionId == null) {
-            throw ErrorUtils.sendError("Must provide a resource ID and a distribution ID.",
-                    Response.Status.BAD_REQUEST);
-        }
-
-        Optional<PublishedResource> publishedResourceOptional =
-                catalogManager.getResource(values.getIriOrBnode(resourceId));
-
-        // If resource and distribution are present, return distribution
-        if (publishedResourceOptional.isPresent()) {
-            PublishedResource publishedResource = publishedResourceOptional.get();
-
-            Optional<Distribution> distributionOptional = checkForDistribution(publishedResource, distributionId);
-
-            if (distributionOptional.isPresent()) {
-                return processDistribution(distributionOptional.get());
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public Response deleteDistribution(String resourceId, String distributionId) {
+    public Response getRecord(String catalogId, String recordId) {
         return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
-    public Response getResourceTypes() {
-        JSONArray json = new JSONArray();
-
-        RESOURCE_TYPES.forEach(json::add);
-
-        return Response.ok(json.toString()).build();
+    public Response deleteRecord(String catalogId, String recordId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
-    public Response getSortOptions() {
-        JSONArray json = new JSONArray();
-
-        SORT_RESOURCES.forEach(json::add);
-
-        return Response.ok(json.toString()).build();
+    public <T extends Record> Response updateRecord(String catalogId, String recordId, T newRecord) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
-    private XMLGregorianCalendar getCalendar(OffsetDateTime offsetDateTime) {
-        GregorianCalendar calendar = GregorianCalendar.from(offsetDateTime.toZonedDateTime());
-
-        try {
-            return DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
-        } catch (DatatypeConfigurationException e) {
-            throw ErrorUtils.sendError(e, "Unable to serialize resource data", Response.Status.INTERNAL_SERVER_ERROR);
-        }
+    @Override
+    public Response getUnversionedDistributions(String catalogId, String recordId, String sort, int offset, int limit) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
-    private Optional<Distribution> checkForDistribution(PublishedResource resource, String distributionId) {
-        Resource distributionResource = values.getIriOrBnode(distributionId);
-
-        for (Distribution distribution : resource.getDistributions()) {
-            if (distribution.getResource().equals(distributionResource)) {
-                return Optional.of(distribution);
-            }
-        }
-
-        return Optional.empty();
+    @Override
+    public Response createUnversionedDistribution(String catalogId, String recordId, Distribution newDistribution) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
-    private PublishedResourceMarshaller processResource(PublishedResource resource) {
-        PublishedResourceMarshaller marshaller = new PublishedResourceMarshaller();
-        marshaller.setId(resource.getResource().stringValue());
-        marshaller.setTitle(resource.getTitle());
-        marshaller.setDescription(resource.getDescription());
-        marshaller.setIssued(getCalendar(resource.getIssued()));
-        marshaller.setModified(getCalendar(resource.getModified()));
-        marshaller.setIdentifier(resource.getIdentifier());
-
-        Set<String> types = new HashSet<>();
-        resource.getTypes().forEach(res -> types.add(res.stringValue()));
-        marshaller.setTypes(types);
-
-        marshaller.setKeywords(resource.getKeywords());
-
-        Set<String> distributions = new HashSet<>();
-        resource.getDistributions().forEach(distribution ->
-                distributions.add(distribution.getResource().stringValue()));
-        marshaller.setDistributions(distributions);
-
-        return marshaller;
+    @Override
+    public Response getUnversionedDistribution(String catalogId, String recordId, String distributionId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 
-    private DistributionMarshaller processDistribution(Distribution distribution) {
-        DistributionMarshaller marshaller = new DistributionMarshaller();
-        marshaller.setId(distribution.getResource().stringValue());
-        marshaller.setTitle(distribution.getTitle());
-        marshaller.setDescription(distribution.getDescription());
-        marshaller.setIssued(getCalendar(distribution.getIssued()));
-        marshaller.setModified(getCalendar(distribution.getModified()));
-        marshaller.setLicense(distribution.getLicense());
-        marshaller.setRights(distribution.getRights());
+    @Override
+    public Response deleteUnversionedDistribution(String catalogId, String recordId, String distributionId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
 
-        String accessURL = distribution.getAccessURL() == null ? null : distribution.getAccessURL().toString();
-        marshaller.setAccessURL(accessURL);
+    @Override
+    public Response updateUnversionedDistribution(String catalogId, String recordId, String distributionId, 
+                                                  Distribution newDistribution) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
 
-        String downloadURL = distribution.getDownloadURL() == null ? null : distribution.getDownloadURL().toString();
-        marshaller.setDownloadURL(downloadURL);
+    @Override
+    public Response getLatestVersion(String catalogId, String recordId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
 
-        marshaller.setMediaType(distribution.getMediaType());
-        marshaller.setFormat(distribution.getFormat());
-        marshaller.setBytesSize(distribution.getByteSize());
+    @Override
+    public Response getVersions(String catalogId, String recordId, String sort, int offset, int limit) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
 
-        return marshaller;
+    @Override
+    public <T extends Version> Response createVersion(String catalogId, String recordId, T newVersion) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getVersion(String catalogId, String recordId, String versionId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response deleteVersion(String catalogId, String recordId, String versionId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public <T extends Version> Response updateVersion(String catalogId, String recordId, String versionId, 
+                                                      T newVersion) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getVersionedDistributions(String catalogId, String recordId, String versionId, String sort, 
+                                              int offset, int limit) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response createVersionedDistribution(String catalogId, String recordId, String versionId, 
+                                                Distribution newDistribution) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getVersionedDistribution(String catalogId, String recordId, String versionId, 
+                                             String distributionId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response deleteVersionedDistribution(String catalogId, String recordId, String versionId, 
+                                                String distributionId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response updateVersionedDistribution(String catalogId, String recordId, String versionId, 
+                                                String distributionId, Distribution newDistribution) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getVersionCommit(String catalogId, String recordId, String versionId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getMasterBranch(String catalogId, String recordId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getBranches(String catalogId, String recordId, String sort, int offset, int limit) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response createBranch(String catalogId, String recordId, Branch newBranch) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getBranch(String catalogId, String recordId, String branchId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response deleteBranch(String catalogId, String recordId, String branchId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response updateBranch(String catalogId, String recordId, String branchId, Branch newBranch) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getHead(String catalogId, String recordId, String branchId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getCommitChain(String catalogId, String recordId, String branchId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response createBranchCommit(@Context ContainerRequestContext context, String catalogId, String recordId, 
+                                       String branchId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getBranchCommit(String catalogId, String recordId, String branchId, String commitId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getConflicts(String catalogId, String recordId, String branchId, String leftId, String rightId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response merge(String catalogId, String recordId, String branchId, String leftId, String rightId, 
+                          String additionsJson, String deletionsJson) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getCompiledResource(ContainerRequestContext context, String catalogId, String recordId, 
+                                        String branchId, String commitId, String rdfFormat, boolean apply) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response downloadCompiledResource(ContainerRequestContext context, String catalogId, String recordId, 
+                                             String branchId, String commitId, String rdfFormat, boolean apply) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response createInProgressCommit(ContainerRequestContext context, String catalogId, String recordId,
+                                           InProgressCommit newInProgressCommit) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response getInProgressCommit(ContainerRequestContext context, String catalogId, String recordId,
+                                        String rdfFormat) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response deleteInProgressCommit(ContainerRequestContext context, String catalogId, String recordId) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public Response updateInProgressCommit(ContainerRequestContext context, String catalogId, String recordId, 
+                                           String additionsJson, String deletionsJson) {
+        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
     }
 }
