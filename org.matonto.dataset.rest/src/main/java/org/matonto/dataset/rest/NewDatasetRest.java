c8734bae61a2a8237e353695870a665404bb0d1e
Sean Smitz <sean.smitz@inovexcorp.com>
2017-05-08 15:47:47 -0400

    Clean-up and JavaDoc. Integrated discussion items.

--- org.matonto.dataset.rest/src/main/java/org/matonto/dataset/rest/NewDatasetRest.java
+++ org.matonto.dataset.rest/src/main/java/org/matonto/dataset/rest/NewDatasetRest.java
@@ -18,50 +18,55 @@ import io.swagger.annotations.ApiOperation;
 /**
  * Created by seansmitz on 5/5/17.
  */
-@Path("/datasets")
-@Api(value = "/datasets")
+@Path("/explorable-datasets")
+@Api(value = "/explorable-datasets")
 public interface NewDatasetRest {
     /**
-     * Retrieves all the Datasets in the local Catalog in a JSON array. Can optionally be paged if passed a
+     * Retrieves all the data, associated by {@link org.matonto.dataset.ontology.dataset.Dataset}, in the local
+     * {@link org.matonto.catalog.api.ontologies.mcat.Catalog} in a JSON array. Can optionally be paged if passed a
      * limit and offset. Can optionally be sorted by property value if passed a sort IRI.
+     * NOTE: This should <emp>never</emp> be implemented without paging and probably not even then.
      *
-     * @param uriInfo The URI information of the request to be used in creating links to other pages of Datasets
-     * @param offset The offset for a page of Datasets
-     * @param limit The number of Datasets to return in one page
-     * @param sort The IRI of the property to sort by
-     * @param asc Whether or not the list should be sorted ascending or descending. Default is ascending
-     * @param searchText The optional search text for the query
-     * @return A Response with a JSON array of Datasets
+     * @param uriInfo The URI information of the request to be used in creating links to other pages of data
+     * @param offset  The offset for a page of data
+     * @param limit   The number of data to return in one page
+     * @param sort    The IRI of the property to sort by
+     * @param asc     Whether or not the list should be sorted ascending or descending. Default is ascending
+     * @param filter  The optional search text for the query
+     * @return A {@link Response} with a JSON array of data associated by dataset.
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("user")
-    @ApiOperation("Retrieves all Datasets in the local Catalog")
+    @ApiOperation("Retrieves all the data, grouped by Dataset, in the local Catalog")
     Response getDatasets(@Context UriInfo uriInfo,
                          @QueryParam("offset") int offset,
                          @QueryParam("limit") int limit,
                          @QueryParam("sort") String sort,
                          @DefaultValue("true") @QueryParam("ascending") boolean asc,
-                         @QueryParam("searchText") String searchText);
+                         @QueryParam("filter") String filter);
 
     /**
-     * Retrieves all the triples from a Dataset in the local Catalog in a JSON array. Can optionally be paged if passed
-     * a limit and offset. Can optionally be sorted by property value if passed a sort IRI.
+     * Retrieves all the data from a {@link org.matonto.dataset.ontology.dataset.Dataset} in the local
+     * {@link org.matonto.catalog.api.ontologies.mcat.Catalog} in a JSON array. Can optionally be paged if passed a
+     * limit and offset. Can optionally be sorted by property value if passed a sort IRI.
+     * NOTE: This should <emp>never</emp> be implemented without paging and probably not even then.
      *
-     * @param uriInfo The URI information of the request to be used in creating links to other pages of triples
-     * @param datasetId The id of the dataset from which to retrieve the triples.
-     * @param offset The offset for a page of Dataset triples
-     * @param limit The number of triples to return in one page
-     * @param sort The IRI of the property to sort by
-     * @param asc Whether or not the list should be sorted ascending or descending. Default is ascending
-     * @param filter The optional search text for the query
-     * @return A Response with a JSON array of Datasets
+     * @param uriInfo   The URI information of the request to be used in creating links to other pages of data
+     * @param datasetId The id of the {@link org.matonto.dataset.ontology.dataset.DatasetRecord} for the
+     *                  {@link org.matonto.dataset.ontology.dataset.Dataset} from which to retrieve the data.
+     * @param offset    The offset for a page of data
+     * @param limit     The number of data to return in one page
+     * @param sort      The IRI of the property to sort by
+     * @param asc       Whether or not the list should be sorted ascending or descending. Default is ascending
+     * @param filter    The optional search text for the query
+     * @return A {@link Response} with a JSON array of data
      */
     @GET
     @Path("{datasetId}")
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("user")
-    @ApiOperation("Retrieves all triples from a Dataset in the local Catalog")
+    @ApiOperation("Retrieves all data from a Dataset in the local Catalog")
     Response getDatasetData(@Context UriInfo uriInfo,
                             @PathParam("datasetId") String datasetId,
                             @QueryParam("offset") int offset,
@@ -71,23 +76,26 @@ public interface NewDatasetRest {
                             @QueryParam("filter") String filter);
 
     /**
-     * Retrieves all the triples from a Dataset in the local Catalog in a JSON array. Can optionally be paged if passed
-     * a limit and offset. Can optionally be sorted by property value if passed a sort IRI.
+     * Retrieves all the data associated with ontology objects, from a
+     * {@link org.matonto.dataset.ontology.dataset.Dataset} in the local
+     * {@link org.matonto.catalog.api.ontologies.mcat.Catalog} in a JSON array. Can optionally be paged if passed a
+     * limit and offset. Can optionally be sorted by property value if passed a sort IRI.
      *
-     * @param uriInfo The URI information of the request to be used in creating links to other pages of triples
-     * @param datasetId The id of the dataset from which to retrieve the triples.
-     * @param offset The offset for a page of Dataset triples
-     * @param limit The number of triples to return in one page
-     * @param sort The IRI of the property to sort by
-     * @param asc Whether or not the list should be sorted ascending or descending. Default is ascending
-     * @param filter
-     * @return A Response with a JSON array of Datasets
+     * @param uriInfo   The URI information of the request to be used in creating links to other pages of data
+     * @param datasetId The id of the {@link org.matonto.dataset.ontology.dataset.DatasetRecord} for the
+     *                  {@link org.matonto.dataset.ontology.dataset.Dataset} from which to retrieve the data.
+     * @param offset    The offset for a page of ontology objects.
+     * @param limit     The number of ontology objects to return in one page
+     * @param sort      The IRI of the property to sort by
+     * @param asc       Whether or not the list should be sorted ascending or descending. Default is ascending
+     * @param filter    The optional array of ontology object ids to filter by.
+     * @return A {@link Response} with a JSON array of ontology objects.
      */
     @GET
     @Path("{datasetId}/instances")
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("user")
-    @ApiOperation("Retrieves all triples for instances of classes from a Dataset in the local Catalog")
+    @ApiOperation("Retrieves all the data associated with ontology objects, from a Dataset in the local Catalog")
     Response getDatasetDataInstances(@Context UriInfo uriInfo,
                                      @PathParam("datasetId") String datasetId,
                                      @QueryParam("offset") int offset,
@@ -97,53 +105,57 @@ public interface NewDatasetRest {
                                      @QueryParam("filter") String filter);
 
     /**
+     * Retrieves an aggregated summary of all ontology objects from a
+     * {@link org.matonto.dataset.ontology.dataset.Dataset} in the local
+     * {@link org.matonto.catalog.api.ontologies.mcat.Catalog}, grouped by their ontology, as a JSON object.
      *
-     *
-     * @param uriInfo The URI information of the request to be used in creating links to other pages of triples
-     * @param datasetId The id of the dataset from which to retrieve the triples.
-     * @param offset The offset for a page of Dataset triples
-     * @param limit The number of triples to return in one page
-     * @param sort The IRI of the property to sort by
-     * @param asc Whether or not the list should be sorted ascending or descending. Default is ascending
-     * @param numExamples
-     * @return A Response with a JSON array of Datasets
+     * @param uriInfo     The URI information of the request to be used in creating links to other pages of data
+     * @param datasetId   The id of the {@link org.matonto.dataset.ontology.dataset.DatasetRecord} for the
+     *                    {@link org.matonto.dataset.ontology.dataset.Dataset} to summarize.
+     * @param offset      The offset for a page of Dataset data
+     * @param limit       The number of data to return in one page
+     * @param sort        The IRI of the property to sort by
+     * @param asc         Whether or not the list should be sorted ascending or descending. Default is ascending
+     * @param numExamples the maximum number of examples to be provided for each ontology object.
+     * @return A {@link Response} with a JSON object.
      */
     @GET
-    @Path("{datasetId}/instances/summary")
+    @Path("{datasetId}/instances-summary")
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("user")
-    @ApiOperation("Retrieves all triples for instances of classes from a Dataset in the local Catalog")
+    @ApiOperation("Retrieves an aggregated summary of all ontology objects from a Dataset in the local Catalog")
     Response getDatasetDataInstancesSummary(@Context UriInfo uriInfo,
-                                     @PathParam("datasetId") String datasetId,
-                                     @QueryParam("offset") int offset,
-                                     @QueryParam("limit") int limit,
-                                     @QueryParam("sort") String sort,
-                                     @DefaultValue("true") @QueryParam("ascending") boolean asc,
-                                     @QueryParam("numExamples") int numExamples);
+                                            @PathParam("datasetId") String datasetId,
+                                            @QueryParam("offset") int offset,
+                                            @QueryParam("limit") int limit,
+                                            @QueryParam("sort") String sort,
+                                            @DefaultValue("true") @QueryParam("ascending") boolean asc,
+                                            @QueryParam("numExamples") int numExamples);
 
     /**
+     * Retrieves a particular instance of an ontology object from a
+     * {@link org.matonto.dataset.ontology.dataset.Dataset} in the local
+     * {@link org.matonto.catalog.api.ontologies.mcat.Catalog} as a JSON object.
      *
-     *
-     * @param uriInfo The URI information of the request to be used in creating links to other pages of triples
-     * @param datasetId The id of the dataset from which to retrieve the triples.
-     * @param instanceId The id of the class instance to retrieve.
-     * @param offset The offset for a page of Dataset triples
-     * @param limit The number of triples to return in one page
-     * @param sort The IRI of the property to sort by
-     * @param asc Whether or not the list should be sorted ascending or descending. Default is ascending
-     * @return A Response with a JSON array of Datasets
+     * @param uriInfo    The URI information of the request to be used in creating links to other pages of data
+     * @param datasetId  The id of the {@link org.matonto.dataset.ontology.dataset.DatasetRecord} for the
+     *                   {@link org.matonto.dataset.ontology.dataset.Dataset}
+     * @param instanceId The id of the ontology object instance to retrieve.
+     * @param sort       The IRI of the property to sort by
+     * @param asc        Whether or not the list should be sorted ascending or descending. Default is ascending
+     * @return A {@link Response} with a JSON object.
      */
     @GET
     @Path("{datasetId}/instances/{instanceId}")
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("user")
-    @ApiOperation("Retrieves all triples for instances of classes from a Dataset in the local Catalog")
-    Response getDatasetDataInstancesSummary(@Context UriInfo uriInfo,
-                                            @PathParam("datasetId") String datasetId,
-                                            @PathParam("instanceId") String instanceId,
-                                            @QueryParam("offset") int offset,
-                                            @QueryParam("limit") int limit,
-                                            @QueryParam("sort") String sort,
-                                            @DefaultValue("true") @QueryParam("ascending") boolean asc);
+    @ApiOperation("Retrieves a particular instance of an ontology object from a Dataset in the local Catalog")
+    Response getDatasetDataInstance(@Context UriInfo uriInfo,
+                                    @PathParam("datasetId") String datasetId,
+                                    @PathParam("instanceId") String instanceId,
+                                    @QueryParam("offset") int offset,
+                                    @QueryParam("limit") int limit,
+                                    @QueryParam("sort") String sort,
+                                    @DefaultValue("true") @QueryParam("ascending") boolean asc);
 
-    }
+}
