60933ec8b703144a281076f8271f8fc61219e0ad
Ben Gould <ben.gould@inovexcorp.com>
2017-06-21 11:32:07 -0400

    services workin

--- org.matonto.clustering.hazelcast/src/main/java/org/matonto/clustering/hazelcast/HazelcastClusteringService.java
+++ org.matonto.clustering.hazelcast/src/main/java/org/matonto/clustering/hazelcast/HazelcastClusteringService.java
@@ -1,17 +1,69 @@
 package org.matonto.clustering.hazelcast;
 
+/*-
+ * #%L
+ * clustering.hazelcast
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2016 - 2017 iNovex Information Systems, Inc.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.ConfigurationPolicy;
 import aQute.bnd.annotation.component.Deactivate;
+import aQute.bnd.annotation.metatype.Configurable;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.LifecycleEvent;
+import com.hazelcast.core.LifecycleListener;
 import org.apache.commons.lang.StringUtils;
 import org.matonto.clustering.api.ClusteringService;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Component
+import java.util.Map;
+
+/**
+ * This is the {@link ClusteringService} implementation built on top of Hazelcast.
+ */
+@Component(immediate = true,
+        configurationPolicy = ConfigurationPolicy.require,
+        designateFactory = HazelcastClusteringServiceConfig.class,
+        name = HazelcastClusteringService.NAME,
+        provide = ClusteringService.class,
+        properties = {
+                "clusteringType=hazelcast"
+        }
+)
 public class HazelcastClusteringService implements ClusteringService {
+
+    /**
+     * The name of this service type.
+     */
+    static final String NAME = "org.matonto.clustering.hazelcast";
+
+    /**
+     * {@link Logger} for this service.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(HazelcastClusteringService.class);
 
     /**
      * Hazelcast instance that will drive the features of this {@link ClusteringService} implementation.
@@ -19,44 +71,53 @@ public class HazelcastClusteringService implements ClusteringService {
     private HazelcastInstance hazelcastInstance;
 
     /**
-     * Hazelcast configuration object, set up by the service configuration.
+     * {@link BundleContext} for this service.
      */
-    private final Config config = new Config();
-
-    /**
-     * The bundle context for this service.
-     */
-    private final BundleContext bundleContext;
-
-    /**
-     * Construct a new {@link HazelcastClusteringService} instance.
-     *
-     * @param context       The {@link BundleContext}
-     * @param serviceConfig The {@link HazelcastClusteringServiceConfig} service configuration
-     */
-    public HazelcastClusteringService(final BundleContext context,
-                                      final HazelcastClusteringServiceConfig serviceConfig) {
-        this.bundleContext = context;
-        if (StringUtils.isNotBlank(serviceConfig.instanceName())) {
-            config.setInstanceName(serviceConfig.instanceName());
-        }
-        if (serviceConfig.basicPort() > 0) {
-            config.getNetworkConfig().setPort(serviceConfig.basicPort());
-        }
-        if (serviceConfig.multicastPort() > 0) {
-            config.getNetworkConfig().getJoin().getMulticastConfig().setMulticastPort(serviceConfig.multicastPort());
-        }
-        if (serviceConfig.outboundPorts() != null && !serviceConfig.outboundPorts().isEmpty()) {
-            config.getNetworkConfig().setOutboundPorts(serviceConfig.outboundPorts());
-        }
-    }
+    private BundleContext bundleContext;
 
     /**
      * Method that joins the hazelcast cluster when the service is activated.
      */
     @Activate
-    public void activate() {
+    public void activate(BundleContext context, Map<String, Object> configuration) {
+        final HazelcastClusteringServiceConfig serviceConfig = Configurable.createConfigurable(HazelcastClusteringServiceConfig.class, configuration);
+        this.bundleContext = context;
+        final Config config = new Config();
+        config.getNetworkConfig().getJoin().getMulticastConfig().setMulticastGroup("matonto");
+        LOGGER.debug("Spinning up underlying hazelcast instance");
+        LOGGER.info("Initializing Hazelcast based Clustering Service");
+        if (StringUtils.isNotBlank(serviceConfig.instanceName())) {
+            config.setInstanceName(serviceConfig.instanceName());
+            LOGGER.debug("Configured instance name to: {}", serviceConfig.instanceName());
+        }
+        if (serviceConfig.basicPort() > 0) {
+            config.getNetworkConfig().setPort(serviceConfig.basicPort());
+            LOGGER.debug("Configured our base port to: {}", serviceConfig.basicPort());
+        }
+        if (serviceConfig.multicastPort() > 0) {
+            config.getNetworkConfig().getJoin().getMulticastConfig().setMulticastPort(serviceConfig.multicastPort());
+            LOGGER.debug("Configured our multicast port to: {}", serviceConfig.multicastPort());
+        }
+        if (serviceConfig.outboundPorts() != null && !serviceConfig.outboundPorts().isEmpty()) {
+            config.getNetworkConfig().setOutboundPorts(serviceConfig.outboundPorts());
+            LOGGER.debug("Configured our outbound ports to: {}", StringUtils.join(serviceConfig.outboundPorts(), ", "));
+        }
+        if (StringUtils.isNotBlank(serviceConfig.groupConfigName())) {
+            config.getGroupConfig().setName(serviceConfig.groupConfigName());
+            LOGGER.debug("Configured group name to: {}", serviceConfig.groupConfigName());
+        }
+        if (StringUtils.isNotBlank(serviceConfig.groupConfigPassword())) {
+            config.getGroupConfig().setPassword(serviceConfig.groupConfigPassword());
+            LOGGER.debug("Configured group password...");
+        }
         this.hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+        LOGGER.info("Successfully initialized Hazelcast instance");
+        this.hazelcastInstance.getLifecycleService().addLifecycleListener(new LifecycleListener() {
+            @Override
+            public void stateChanged(LifecycleEvent event) {
+                LOGGER.warn("State Change: {}: {}", event.getState().name(), event.toString());
+            }
+        });
     }
 
     /**
@@ -64,6 +125,7 @@ public class HazelcastClusteringService implements ClusteringService {
      */
     @Deactivate
     public void deactivate() {
+        LOGGER.info("Shutting down underlying hazelcast instance");
         this.hazelcastInstance.shutdown();
     }
 
--- org.matonto.clustering.hazelcast/src/main/java/org/matonto/clustering/hazelcast/HazelcastClusteringServiceConfig.java
+++ org.matonto.clustering.hazelcast/src/main/java/org/matonto/clustering/hazelcast/HazelcastClusteringServiceConfig.java
@@ -1,11 +1,34 @@
 package org.matonto.clustering.hazelcast;
 
+/*-
+ * #%L
+ * clustering.hazelcast
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2016 - 2017 iNovex Information Systems, Inc.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import aQute.bnd.annotation.metatype.Meta;
 import org.matonto.clustering.api.ClusteringServiceConfig;
 
 import java.util.Set;
 
-
+@Meta.OCD(factory = true)
 public interface HazelcastClusteringServiceConfig extends ClusteringServiceConfig {
 
     @Meta.AD(required = false)
@@ -19,5 +42,11 @@ public interface HazelcastClusteringServiceConfig extends ClusteringServiceConfi
 
     @Meta.AD(required = false)
     Set<Integer> outboundPorts();
+
+    @Meta.AD(required = false)
+    String groupConfigName();
+
+    @Meta.AD(required = false)
+    String groupConfigPassword();
 
 }
