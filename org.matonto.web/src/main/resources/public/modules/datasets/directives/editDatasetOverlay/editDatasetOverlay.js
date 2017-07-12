93af22807707c86eebc69f0cf95a2497b964f3d5
Sean Smitz <sean.smitz@inovexcorp.com>
2017-07-12 11:39:35 -0400

    Ontology list is now populated automatically.

--- org.matonto.web/src/main/resources/public/modules/datasets/directives/editDatasetOverlay/editDatasetOverlay.js
+++ org.matonto.web/src/main/resources/public/modules/datasets/directives/editDatasetOverlay/editDatasetOverlay.js
@@ -190,6 +190,8 @@
 
                         return angular.fromJson(jsonString);
                     }
+                    
+                    dvm.getOntologies();
                 }
             }
         }
