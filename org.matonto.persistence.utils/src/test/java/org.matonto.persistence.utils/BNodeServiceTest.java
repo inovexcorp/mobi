f258ab1cb0aa7b47556c1e9636b7166bfa1788f4
Jordan Lewis <jordan.lewis@inovexcorp.com>
2017-07-20 12:15:56 -0400

    Removing some unneeded changes.

--- org.matonto.persistence.utils/src/test/java/org.matonto.persistence.utils/BNodeServiceTest.java
+++ org.matonto.persistence.utils/src/test/java/org.matonto.persistence.utils/BNodeServiceTest.java
@@ -24,12 +24,7 @@ package org.matonto.persistence.utils;
  */
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
