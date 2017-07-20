5781e6adb4a08d5f564d635afc772bd48958d411
Ben Gould <ben.gould@inovexcorp.com>
2017-07-20 11:55:08 -0400

    added negative test

--- rdf-orm/org.matonto.rdf.orm/src/test/java/org/matonto/rdf/orm/conversion/impl/LongValueConverterTest.java
+++ rdf-orm/org.matonto.rdf.orm/src/test/java/org/matonto/rdf/orm/conversion/impl/LongValueConverterTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
+import org.matonto.rdf.orm.conversion.ValueConversionException;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class LongValueConverterTest extends ValueConverterTestCase<Long> {
@@ -17,5 +18,11 @@ public class LongValueConverterTest extends ValueConverterTestCase<Long> {
         TestCase.assertEquals(10L,
                 (long) valueConverter.convertValue(valueConverter.convertType(10L, null), null, Long.class));
     }
+
+    @Test(expected = ValueConversionException.class)
+    public void badTest() {
+        valueConverter.convertValue(valueFactory.createLiteral("3.141592"), null, Long.class);
+    }
+
 
 }
