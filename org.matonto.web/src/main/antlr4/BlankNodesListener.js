0a8b1bc06cfc3c7063f6c5b4e3a722ddae3945e4
Megan Mercer <megan.mercer@inovexcorp.com>
2017-07-25 12:37:47 -0400

    Remove generated files from src directory so that they reprocess on a new build.

--- org.matonto.web/src/main/antlr4/BlankNodesListener.js
+++ org.matonto.web/src/main/antlr4/BlankNodesListener.js
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-var MOSListener = require('./generated/MOSListener');
+var MOSListener = require('./../../../target/generated-sources/antlr4/MOSListener');
 
 var prefixes, util;
 
--- org.matonto.web/src/main/antlr4/manchester.js
+++ org.matonto.web/src/main/antlr4/manchester.js
@@ -21,7 +21,7 @@
  * #L%
  */
 exports.antlr4 = require('antlr4/index');
-exports.MOSLexer = require('./generated/MOSLexer');
-exports.MOSParser = require('./generated/MOSParser');
+exports.MOSLexer = require('./../../../target/generated-sources/antlr4/MOSLexer');
+exports.MOSParser = require('./../../../target/generated-sources/antlr4/MOSParser');
 exports.BlankNodesListener = require('./BlankNodesListener').BlankNodesListener;
 exports.BlankNodesErrorListener = require('./BlankNodesErrorListener').BlankNodesErrorListener;
