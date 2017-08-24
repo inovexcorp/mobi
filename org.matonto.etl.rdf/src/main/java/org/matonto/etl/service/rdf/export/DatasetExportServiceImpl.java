73fcb24df6aa3dc541be4294bf88dcbd7ca899bc
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2017-08-24 17:40:18 -0400

    Updated dataset stuff

--- org.matonto.etl.rdf/src/main/java/org/matonto/etl/service/rdf/export/DatasetExportServiceImpl.java
+++ org.matonto.etl.rdf/src/main/java/org/matonto/etl/service/rdf/export/DatasetExportServiceImpl.java
@@ -1,5 +1,29 @@
 package org.matonto.etl.service.rdf.export;
 
+/*-
+ * #%L
+ * org.matonto.etl.rdf
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2016 - 2017 iNovex Information Systems, Inc.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Reference;
 import org.matonto.dataset.api.DatasetConnection;
 import org.matonto.dataset.api.DatasetManager;
@@ -24,6 +48,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class DatasetExportServiceImpl implements DatasetExportService {
 
     private static final Logger LOG = LoggerFactory.getLogger(DatasetExportServiceImpl.class);
