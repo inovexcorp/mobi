210a348f5c111be2534025314f46a014bb84655e
Sean Smitz <sean.smitz@inovexcorp.com>
2017-09-21 15:31:35 -0400

    Found and fixed some actual issues.

--- org.matonto.catalog.impl/src/main/java/org/matonto/catalog/impl/CatalogProvUtilsImpl.java
+++ org.matonto.catalog.impl/src/main/java/org/matonto/catalog/impl/CatalogProvUtilsImpl.java
@@ -117,6 +117,7 @@ public class CatalogProvUtilsImpl implements CatalogProvUtils {
     @Override
     public void endCreateActivity(CreateActivity createActivity, Resource recordIRI) {
         Entity recordEntity = entityFactory.createNew(recordIRI, createActivity.getModel());
+        recordEntity.addGeneratedAtTime(OffsetDateTime.now());
         recordEntity.addProperty(vf.createLiteral(catalogManager.getRepositoryId()), vf.createIRI(atLocation));
         createActivity.addGenerated(recordEntity);
         finalizeActivity(createActivity);
@@ -140,7 +141,9 @@ public class CatalogProvUtilsImpl implements CatalogProvUtils {
 
     @Override
     public void endDeleteActivity(DeleteActivity deleteActivity, Record record) {
-        Entity recordEntity = entityFactory.createNew(record.getResource(), deleteActivity.getModel());
+        Entity recordEntity = entityFactory.getExisting(record.getResource(), deleteActivity.getModel()).orElseThrow(()
+                -> new IllegalStateException("No Entity found for record."));;
+        recordEntity.addInvalidatedAtTime(OffsetDateTime.now());
 
         finalizeActivity(deleteActivity);
 
@@ -158,17 +161,14 @@ public class CatalogProvUtilsImpl implements CatalogProvUtils {
     }
 
     private Activity initializeActivity(ActivityConfig config) {
-        OffsetDateTime start = OffsetDateTime.now();
-
         Activity activity = provenanceService.createActivity(config);
-        activity.addStartedAtTime(start);
+        activity.addStartedAtTime(OffsetDateTime.now());
         activity.addProperty(vf.createLiteral(matOnto.getServerIdentifier().toString()), vf.createIRI(atLocation));
 
         return activity;
     }
 
     private void finalizeActivity(Activity activity) {
-        OffsetDateTime stop = OffsetDateTime.now();
-        activity.addEndedAtTime(stop);
+        activity.addEndedAtTime(OffsetDateTime.now());
     }
 }
