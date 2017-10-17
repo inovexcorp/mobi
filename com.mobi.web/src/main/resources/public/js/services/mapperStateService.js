0b7e38496f618fb9f3e84d3eaf1331a6b3402304
Jordan Lewis <jordan.lewis@inovexcorp.com>
2017-10-17 17:23:27 -0400

    Updating comment and test.

--- com.mobi.web/src/main/resources/public/js/services/mapperStateService.js
+++ com.mobi.web/src/main/resources/public/js/services/mapperStateService.js
@@ -10,12 +10,12 @@
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -661,6 +661,7 @@
              * @param {string} propId The id of the property that was changed
              * @param {string} newValue The new value of the property
              * @param {string} originalValue The original value of the property
+             * @param {boolean} isId True if it has an '@id'
              */
             self.changeProp = function(entityId, propId, newValue, originalValue, isId = false) {
                 if (newValue !== originalValue) {
