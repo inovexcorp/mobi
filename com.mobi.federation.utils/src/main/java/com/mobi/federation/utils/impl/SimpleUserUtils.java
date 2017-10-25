299ba50516273a5492f8385d18fa72bba2c685b1
Jordan Lewis <jordan.lewis@inovexcorp.com>
2017-10-25 11:17:46 -0400

    Made variable package private.

--- com.mobi.federation.utils/src/main/java/com/mobi/federation/utils/impl/SimpleUserUtils.java
+++ com.mobi.federation.utils/src/main/java/com/mobi/federation/utils/impl/SimpleUserUtils.java
@@ -44,7 +44,7 @@ import javax.security.auth.login.LoginException;
 
 @Component
 public class SimpleUserUtils implements UserUtils {
-    protected static final String FEDERATION_USERS_KEY = "federation.users";
+    static final String FEDERATION_USERS_KEY = "federation.users";
 
     private UserFactory userFactory;
     private ValueFactory vf;
