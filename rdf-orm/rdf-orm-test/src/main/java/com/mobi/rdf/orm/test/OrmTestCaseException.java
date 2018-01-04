7c78c20f7f60947e2d814dfbea8a499bb4f20636
Ben Gould <ben.gould@inovexcorp.com>
2018-01-04 18:50:09 -0500

    more javadoc

--- rdf-orm/rdf-orm-test/src/main/java/com/mobi/rdf/orm/test/OrmTestCaseException.java
+++ rdf-orm/rdf-orm-test/src/main/java/com/mobi/rdf/orm/test/OrmTestCaseException.java
@@ -1,5 +1,11 @@
 package com.mobi.rdf.orm.test;
 
+/**
+ * This {@link RuntimeException} extension represents when there is an issue that occurs in the
+ * {@link OrmEnabledTestCase}.  When making use of this framework, this exception may be thrown from either the
+ * initialization of the {@link com.mobi.rdf.orm.OrmFactory} system initialization, or when trying to work
+ * with the {@link com.mobi.rdf.orm.OrmFactory}s in your runtime.
+ */
 public class OrmTestCaseException extends RuntimeException {
 
     public OrmTestCaseException(String msg) {
