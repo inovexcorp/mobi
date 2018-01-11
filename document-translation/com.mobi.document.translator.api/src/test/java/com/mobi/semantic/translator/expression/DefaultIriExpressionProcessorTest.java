07cadc6f737779003f4202b218c17cbefc7b4859
Ben Gould <ben.gould@inovexcorp.com>
2018-01-11 09:29:54 -0500

    make use of new orm test harness

--- document-translation/com.mobi.document.translator.api/src/test/java/com/mobi/semantic/translator/expression/DefaultIriExpressionProcessorTest.java
+++ document-translation/com.mobi.document.translator.api/src/test/java/com/mobi/semantic/translator/expression/DefaultIriExpressionProcessorTest.java
@@ -12,129 +12,47 @@ package com.mobi.semantic.translator.expression;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
 import com.mobi.rdf.api.IRI;
-import com.mobi.rdf.api.ModelFactory;
-import com.mobi.rdf.api.ValueFactory;
-import com.mobi.rdf.core.impl.sesame.LinkedHashModelFactoryService;
-import com.mobi.rdf.core.impl.sesame.ValueFactoryService;
-import com.mobi.rdf.orm.conversion.ValueConverterRegistry;
-import com.mobi.rdf.orm.conversion.impl.BigIntegerValueConverter;
-import com.mobi.rdf.orm.conversion.impl.BooleanValueConverter;
-import com.mobi.rdf.orm.conversion.impl.CalendarValueConverter;
-import com.mobi.rdf.orm.conversion.impl.DateValueConverter;
-import com.mobi.rdf.orm.conversion.impl.DefaultValueConverterRegistry;
-import com.mobi.rdf.orm.conversion.impl.DoubleValueConverter;
-import com.mobi.rdf.orm.conversion.impl.FloatValueConverter;
-import com.mobi.rdf.orm.conversion.impl.IRIValueConverter;
-import com.mobi.rdf.orm.conversion.impl.IntegerValueConverter;
-import com.mobi.rdf.orm.conversion.impl.LiteralValueConverter;
-import com.mobi.rdf.orm.conversion.impl.LongValueConverter;
-import com.mobi.rdf.orm.conversion.impl.ResourceValueConverter;
-import com.mobi.rdf.orm.conversion.impl.ShortValueConverter;
-import com.mobi.rdf.orm.conversion.impl.StringValueConverter;
-import com.mobi.rdf.orm.conversion.impl.ValueValueConverter;
-import com.mobi.rdf.orm.impl.ThingFactory;
+import com.mobi.rdf.orm.test.OrmEnabledTestCase;
 import com.mobi.semantic.translator.SemanticTranslationException;
 import com.mobi.semantic.translator.expression.context.ClassIriExpressionContext;
 import com.mobi.semantic.translator.expression.context.PropertyIriExpressionContext;
 import com.mobi.semantic.translator.expression.context.impl.DefaultClassIriExpressionContext;
 import com.mobi.semantic.translator.expression.context.impl.DefaultPropertyIriExpressionContext;
-import com.mobi.semantic.translator.ontology.ExtractedClassFactory;
-import com.mobi.semantic.translator.ontology.ExtractedDatatypePropertyFactory;
-import com.mobi.semantic.translator.ontology.ExtractedObjectPropertyFactory;
 import com.mobi.semantic.translator.ontology.ExtractedOntology;
-import com.mobi.semantic.translator.ontology.ExtractedOntologyFactory;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class DefaultIriExpressionProcessorTest {
+public class DefaultIriExpressionProcessorTest extends OrmEnabledTestCase {
 
     private static final String ONT_URI = "urn://mobi.com/ontologies/testExtraction";
 
-    private static final ThingFactory TF = new ThingFactory();
-
-    private static final ModelFactory MF = new LinkedHashModelFactoryService();
-
-    private static final ValueFactory VF = new ValueFactoryService();
-
-    private static final ExtractedOntologyFactory EXTRACTED_ONTOLOGY_FACTORY = new ExtractedOntologyFactory();
-    private static final ExtractedClassFactory EXTRACTED_CLASS_FACTORY = new ExtractedClassFactory();
-    private static final ExtractedDatatypePropertyFactory EXTRACTED_DATATYPE_PROPERTY_FACTORY =
-            new ExtractedDatatypePropertyFactory();
-    private static final ExtractedObjectPropertyFactory EXTRACTED_OBJECT_PROPERTY_FACTORY =
-            new ExtractedObjectPropertyFactory();
-
     private DefaultIriExpressionProcessor processor;
-
-    @BeforeClass
-    public static void beforeClass() {
-        final ValueConverterRegistry valueConverterRegistry = new DefaultValueConverterRegistry();
-        valueConverterRegistry.registerValueConverter(new BigIntegerValueConverter());
-        valueConverterRegistry.registerValueConverter(new BooleanValueConverter());
-        valueConverterRegistry.registerValueConverter(new CalendarValueConverter());
-        valueConverterRegistry.registerValueConverter(new DateValueConverter());
-        valueConverterRegistry.registerValueConverter(new DoubleValueConverter());
-        valueConverterRegistry.registerValueConverter(new FloatValueConverter());
-        valueConverterRegistry.registerValueConverter(new IntegerValueConverter());
-        valueConverterRegistry.registerValueConverter(new IRIValueConverter());
-        valueConverterRegistry.registerValueConverter(new LiteralValueConverter());
-        valueConverterRegistry.registerValueConverter(new LongValueConverter());
-        valueConverterRegistry.registerValueConverter(new ResourceValueConverter());
-        valueConverterRegistry.registerValueConverter(new ShortValueConverter());
-        valueConverterRegistry.registerValueConverter(new StringValueConverter());
-        valueConverterRegistry.registerValueConverter(new ValueValueConverter());
-
-        EXTRACTED_OBJECT_PROPERTY_FACTORY.setModelFactory(MF);
-        EXTRACTED_OBJECT_PROPERTY_FACTORY.setValueConverterRegistry(valueConverterRegistry);
-        EXTRACTED_OBJECT_PROPERTY_FACTORY.setValueFactory(VF);
-
-        EXTRACTED_DATATYPE_PROPERTY_FACTORY.setModelFactory(MF);
-        EXTRACTED_DATATYPE_PROPERTY_FACTORY.setValueConverterRegistry(valueConverterRegistry);
-        EXTRACTED_DATATYPE_PROPERTY_FACTORY.setValueFactory(VF);
-
-        EXTRACTED_ONTOLOGY_FACTORY.setModelFactory(MF);
-        EXTRACTED_ONTOLOGY_FACTORY.setValueConverterRegistry(valueConverterRegistry);
-        EXTRACTED_ONTOLOGY_FACTORY.setValueFactory(VF);
-
-        EXTRACTED_CLASS_FACTORY.setModelFactory(MF);
-        EXTRACTED_CLASS_FACTORY.setValueConverterRegistry(valueConverterRegistry);
-        EXTRACTED_CLASS_FACTORY.setValueFactory(VF);
-
-        TF.setModelFactory(MF);
-        TF.setValueFactory(new ValueFactoryService());
-        TF.setValueConverterRegistry(valueConverterRegistry);
-        valueConverterRegistry.registerValueConverter(TF);
-        valueConverterRegistry.registerValueConverter(EXTRACTED_OBJECT_PROPERTY_FACTORY);
-        valueConverterRegistry.registerValueConverter(EXTRACTED_DATATYPE_PROPERTY_FACTORY);
-        valueConverterRegistry.registerValueConverter(EXTRACTED_ONTOLOGY_FACTORY);
-        valueConverterRegistry.registerValueConverter(EXTRACTED_CLASS_FACTORY);
-    }
 
     @Before
     public void initProcessor() {
         this.processor = new DefaultIriExpressionProcessor();
-        this.processor.setValueFactory(VF);
+        this.processor.setValueFactory(VALUE_FACTORY);
     }
 
     @Test
     public void testClassIriExpression() throws Exception {
-        ExtractedOntology ont = EXTRACTED_ONTOLOGY_FACTORY.createNew(VF.createIRI(ONT_URI));
+        ExtractedOntology ont = getRequiredOrmFactory(ExtractedOntology.class).createNew(VALUE_FACTORY.createIRI(ONT_URI));
         ClassIriExpressionContext context = new DefaultClassIriExpressionContext(ont, "test-ontology",
                 "simple-test-ontology-for-iri-expressions");
         String expression = "getOntologyIri().concat('#').concat(getName()).concat('/').concat(getComment())";
@@ -145,11 +63,11 @@ public class DefaultIriExpressionProcessorTest {
 
     @Test
     public void testPropertyIriExpression() throws Exception {
-        ExtractedOntology ont = EXTRACTED_ONTOLOGY_FACTORY.createNew(VF.createIRI(ONT_URI));
+        ExtractedOntology ont = getRequiredOrmFactory(ExtractedOntology.class).createNew(VALUE_FACTORY.createIRI(ONT_URI));
         PropertyIriExpressionContext context =
                 new DefaultPropertyIriExpressionContext(ont, "test-ontology",
-                        "simple-test-ontology-for-iri-expressions", VF.createIRI("urn://domain"),
-                        VF.createIRI("urn://range"));
+                        "simple-test-ontology-for-iri-expressions", VALUE_FACTORY.createIRI("urn://domain"),
+                        VALUE_FACTORY.createIRI("urn://range"));
         String expression = "getOntologyIri().concat('#').concat(getName()).concat('/').concat(getComment())";
         IRI result = processor.processExpression(expression, context);
         Assert.assertEquals(ONT_URI + "#test-ontology/simple-test-ontology-for-iri-expressions",
@@ -162,10 +80,10 @@ public class DefaultIriExpressionProcessorTest {
 
     @Test
     public void testBadExpression() throws Exception {
-        ExtractedOntology ont = EXTRACTED_ONTOLOGY_FACTORY.createNew(VF.createIRI(ONT_URI));
+        ExtractedOntology ont = getRequiredOrmFactory(ExtractedOntology.class).createNew(VALUE_FACTORY.createIRI(ONT_URI));
         PropertyIriExpressionContext context = new DefaultPropertyIriExpressionContext(ont, "test-ontology",
-                "simple-test-ontology-for-iri-expressions", VF.createIRI("urn://domain"),
-                VF.createIRI("urn://range"));
+                "simple-test-ontology-for-iri-expressions", VALUE_FACTORY.createIRI("urn://domain"),
+                VALUE_FACTORY.createIRI("urn://range"));
         try {
             processor.processExpression("getNoSuchMethod()", context);
             Assert.fail("Exception should have been thrown when hitting a non-existent method");
