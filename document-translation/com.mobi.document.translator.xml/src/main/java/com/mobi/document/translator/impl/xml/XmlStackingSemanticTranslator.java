0f71413a636cc8fe6090a18523c269a989c95ace
Ben Gould <ben.gould@inovexcorp.com>
2018-01-14 15:35:28 -0500

    cleaning up

--- document-translation/com.mobi.document.translator.xml/src/main/java/com/mobi/document/translator/impl/xml/XmlStackingSemanticTranslator.java
+++ document-translation/com.mobi.document.translator.xml/src/main/java/com/mobi/document/translator/impl/xml/XmlStackingSemanticTranslator.java
@@ -75,67 +75,27 @@ public class XmlStackingSemanticTranslator extends AbstractStackingSemanticTrans
                 final String address = getCurrentLocation();
                 switch (event) {
                     case XMLStreamConstants.START_ELEMENT:
-                        final XmlStackItem item = pushStack(new XmlStackItem(reader.getLocalName(),
-                                !peekStack().isPresent()));
-                        LOG.debug("Thing: '{}' at: {}", item.getIdentifier(), getCurrentLocation());
-                        item.setClassIri(generateClassIri(managedOntology, item.getIdentifier(), getCurrentLocation()));
-                        for (int i = 0; i < reader.getAttributeCount(); i++) {
-                            final String attributeProperty =
-                                    String.format(ATTRIBUTE_PROPERTY_NAME_TEMPLATE, item.getIdentifier(),
-                                            reader.getAttributeName(i));
-                            final ExtractedDatatypeProperty attrProp =
-                                    getOrCreateDatatypeProperty(managedOntology, item.getClassIri(), xsdString(),
-                                            attributeProperty, getCurrentLocation());
-                            item.getProperties().add((IRI) attrProp.getResource(),
-                                    valueFactory.createLiteral(reader.getAttributeValue(i)));
-                        }
+                        // Process the start of an element.
+                        startElement(reader, managedOntology);
                         break;
                     case XMLStreamConstants.COMMENT:
-                        final String commentText = reader.getText();
-                        final Optional<XmlStackItem> opt = peekStack();
-                        if (opt.isPresent()) {
-                            addComment(opt.get(), commentText);
-                        } else {
-                            rootComments.add(commentText);
-                        }
+                        // Handle a comment on the document.
+                        handleComment(reader.getText());
                         break;
                     case XMLStreamConstants.END_ELEMENT:
-                        final XmlStackItem offItem = popStack()
+                        // Pull the item corresponding to this element end off the stack.
+                        final XmlStackItem endingItem = popStack()
                                 // End of element requires a corresponding stack item.
                                 .orElseThrow(() -> new SemanticTranslationException("Element ending without a " +
                                         "corresponding stack item."));
-                        String val = stringBuffer.toString();
-                        LOG.trace("Ending '{}' - Value: '{}'", reader.getLocalName(), val);
+                        // Get the content for this ending element.
+                        final String val = stringBuffer.toString();
                         stringBuffer.setLength(0);
-                        if (offItem.isRoot()) {
-                            rootComments.forEach(comment -> {
-                                addComment(offItem, comment);
-                            });
-                        }
-
-                        if (offItem.getProperties().isEmpty()) {
-                            if (StringUtils.isNotBlank(val)) {
-                                Optional<XmlStackItem> optParent = peekStack();
-                                if (optParent.isPresent()) {
-                                    final XmlStackItem parent = optParent.get();
-                                    final ExtractedDatatypeProperty datatypeProperty = getOrCreateDatatypeProperty(
-                                            managedOntology, parent.getClassIri(), xsdString(),
-                                            offItem.getIdentifier(), getCurrentLocation());
-                                    parent.getProperties().add((IRI) datatypeProperty.getResource(),
-                                            valueFactory.createLiteral(val));
-                                } else {
-                                    LOG.warn("Datatype Property not attached to object...");
-                                }
-                            }
-                        } else {
-                            final ExtractedClass clazz = getOrCreateClass(managedOntology,
-                                    offItem.getClassIri(), offItem.getIdentifier(), address);
-                            if (StringUtils.isNotBlank(val)) {
-                                offItem.getProperties().add(getRdfValue(), valueFactory.createLiteral(val));
-                            }
-                            final IRI instanceIri = createInstance(resultsModel, managedOntology, offItem, clazz, peekStack());
-                            LOG.debug("Created instance of '{}' - '{}'", clazz.getResource(), instanceIri);
-                        }
+                        LOG.trace("Ending '{}' - Value: '{}'", reader.getLocalName(), val);
+                        // Handle the end of a root element.
+                        rootElementEnding(endingItem);
+                        // Handle the ending element.
+                        handleElementEnd(endingItem, val, managedOntology, address, resultsModel);
                         break;
                     case XMLStreamConstants.CHARACTERS:
                         stringBuffer.append(reader.getText().trim());
@@ -150,6 +110,68 @@ public class XmlStackingSemanticTranslator extends AbstractStackingSemanticTrans
             silentlyCloseReader(reader);
         }
         return resultsModel;
+    }
+
+    private void handleElementEnd(XmlStackItem endingItem, String val, ExtractedOntology managedOntology,
+                                  String address, Model resultsModel) throws SemanticTranslationException {
+        if (endingItem.getProperties().isEmpty()) {
+            if (StringUtils.isNotBlank(val)) {
+                Optional<XmlStackItem> optParent = peekStack();
+                if (optParent.isPresent()) {
+                    final XmlStackItem parent = optParent.get();
+                    final ExtractedDatatypeProperty datatypeProperty = getOrCreateDatatypeProperty(
+                            managedOntology, parent.getClassIri(), xsdString(),
+                            endingItem.getIdentifier(), getCurrentLocation());
+                    parent.getProperties().add((IRI) datatypeProperty.getResource(),
+                            valueFactory.createLiteral(val));
+                } else {
+                    LOG.warn("Datatype Property not attached to object...");
+                }
+            }
+        } else {
+            final ExtractedClass clazz = getOrCreateClass(managedOntology,
+                    endingItem.getClassIri(), endingItem.getIdentifier(), address);
+            if (StringUtils.isNotBlank(val)) {
+                endingItem.getProperties().add(getRdfValue(), valueFactory.createLiteral(val));
+            }
+            final IRI instanceIri = createInstance(resultsModel, managedOntology, endingItem, clazz, peekStack());
+            LOG.debug("Created instance of '{}' - '{}'", clazz.getResource(), instanceIri);
+        }
+    }
+
+    private void rootElementEnding(XmlStackItem endingItem) {
+        if (endingItem.isRoot()) {
+            rootComments.forEach(comment -> {
+                addComment(endingItem, comment);
+            });
+        }
+    }
+
+    private void handleComment(String commentText) {
+        final Optional<XmlStackItem> opt = peekStack();
+        if (opt.isPresent()) {
+            addComment(opt.get(), commentText);
+        } else {
+            rootComments.add(commentText);
+        }
+    }
+
+    private void startElement(XMLStreamReader reader, ExtractedOntology managedOntology)
+            throws SemanticTranslationException {
+        final XmlStackItem item = pushStack(new XmlStackItem(reader.getLocalName(),
+                !peekStack().isPresent()));
+        LOG.debug("Thing: '{}' at: {}", item.getIdentifier(), getCurrentLocation());
+        item.setClassIri(generateClassIri(managedOntology, item.getIdentifier(), getCurrentLocation()));
+        for (int i = 0; i < reader.getAttributeCount(); i++) {
+            final String attributeProperty =
+                    String.format(ATTRIBUTE_PROPERTY_NAME_TEMPLATE, item.getIdentifier(),
+                            reader.getAttributeName(i));
+            final ExtractedDatatypeProperty attrProp =
+                    getOrCreateDatatypeProperty(managedOntology, item.getClassIri(), xsdString(),
+                            attributeProperty, getCurrentLocation());
+            item.getProperties().add((IRI) attrProp.getResource(),
+                    valueFactory.createLiteral(reader.getAttributeValue(i)));
+        }
     }
 
     private void silentlyCloseReader(final XMLStreamReader reader) {
