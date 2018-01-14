b5e1799542495a60cf10d828923070a8854712b5
Ben Gould <ben.gould@inovexcorp.com>
2018-01-14 11:11:41 -0500

    initial xml

---
+++ document-translation/com.mobi.document.translator.xml/src/main/java/com/mobi/document/translator/impl/xml/XmlStackItem.java
@@ -0,0 +1,10 @@
+package com.mobi.document.translator.impl.xml;
+
+import com.mobi.document.translator.stack.AbstractStackItem;
+
+public class XmlStackItem extends AbstractStackItem {
+
+    protected XmlStackItem(String id, boolean root) {
+        super(id, root);
+    }
+}
