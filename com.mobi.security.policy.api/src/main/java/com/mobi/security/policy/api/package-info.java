151e779af14fc4fe4df289697bfb4542002e4d57
Megan Mercer <megan.mercer@inovexcorp.com>
2018-02-08 14:03:18 -0500

    Add package info files. Fix Export-Packages for ontology services.

--- com.mobi.security.policy.api/src/main/java/com/mobi/security/policy/api/package-info.java
+++ com.mobi.security.policy.api/src/main/java/com/mobi/security/policy/api/package-info.java
@@ -1,3 +1,5 @@
+@Version("1.0.0.${build}")
+
 package com.mobi.security.policy.api;
 
 /*-
@@ -23,32 +25,4 @@ package com.mobi.security.policy.api;
  * #L%
  */
 
-import com.mobi.rdf.api.IRI;
-
-public class BasicAttributeDesignator implements AttributeDesignator {
-    private IRI attributeId;
-    private IRI category;
-    private IRI datatype;
-
-
-    public BasicAttributeDesignator(IRI attributeId, IRI category, IRI datatype) {
-        this.attributeId = attributeId;
-        this.category = category;
-        this.datatype = datatype;
-    }
-
-    @Override
-    public IRI attributeId() {
-        return null;
-    }
-
-    @Override
-    public IRI category() {
-        return null;
-    }
-
-    @Override
-    public IRI datatype() {
-        return null;
-    }
-}
+import aQute.bnd.annotation.Version;
