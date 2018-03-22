ee4146de4b0086a3bf588b1bdf176b78e08fc2ce
Tom Dalton <tomdalton@Toms-MacBook-Pro.local>
2018-03-22 07:49:52 -0600

    Initial VersionedRDFRecordService and VersionedRDFRecordServiceTest

--- com.mobi.catalog.impl/src/main/java/com/mobi/catalog/impl/record/VersionedRDFRecordService.java
+++ com.mobi.catalog.impl/src/main/java/com/mobi/catalog/impl/record/VersionedRDFRecordService.java
@@ -5,24 +5,19 @@ import com.mobi.catalog.api.CatalogProvUtils;
 import com.mobi.catalog.api.CatalogUtilsService;
 import com.mobi.catalog.api.builder.Difference;
 import com.mobi.catalog.api.ontologies.mcat.*;
-import com.mobi.catalog.api.record.RecordService;
-import com.mobi.catalog.api.record.config.RecordExportConfig;
 import com.mobi.jaas.api.ontologies.usermanagement.User;
-import com.mobi.persistence.utils.BatchExporter;
 import com.mobi.persistence.utils.api.SesameTransformer;
 import com.mobi.rdf.api.IRI;
 import com.mobi.rdf.api.Resource;
 import com.mobi.rdf.api.ValueFactory;
 import com.mobi.repository.api.RepositoryConnection;
-import org.openrdf.rio.Rio;
-import org.openrdf.rio.helpers.BufferedGroupingRDFHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class VersionedRDFRecordService implements RecordService<VersionedRDFRecord> {
+public class VersionedRDFRecordService extends SimpleRecordService {
     private static final Logger LOG = LoggerFactory.getLogger(SimpleRecordService.class);
 
     private CatalogUtilsService utilsService;
@@ -69,46 +64,44 @@ public class VersionedRDFRecordService implements RecordService<VersionedRDFReco
     }
 
     @Override
-    public VersionedRDFRecord delete(IRI catalogId, IRI recordId, User user, RepositoryConnection conn) {
+    public VersionedRDFRecord delete(IRI recordId, User user, RepositoryConnection conn) {
         return null;
     }
 
     @Override
-    public <T extends RecordExportConfig> void export(IRI iriRecord, T config, RepositoryConnection conn) {
-        BatchExporter writer = new BatchExporter(transformer, new BufferedGroupingRDFHandler(Rio.createWriter(config.getFormat(), config.getOutput())));
-        writer.setLogger(LOG);
-        writer.setPrintToSystem(true);
-
+    public void exportRecord(IRI iriRecord, ExportWriter writer, RepositoryConnection conn) {
         VersionedRDFRecord record = utilsService.getExpectedObject(iriRecord, versionedRDFRecordFactory, conn);
-        Catalog catalog = record.getCatalog().orElseThrow(()
-                -> new IllegalArgumentException("Record " + record.getResource() + " does not contain Catalog property"));
 
         Set<Resource> processedCommits = new HashSet<>();
         // Write Branches
         record.getBranch_resource().forEach(branchResource -> {
 
-
             Branch branch = utilsService.getBranch(record, branchResource, branchFactory, conn);
             branch.getModel().forEach(writer::handleStatement);
 
-            // Write Commits
-            for (Resource commitId : utilsService.getCommitChain(catalog.getResource(), false, conn)) {
-
-                if (processedCommits.contains(commitId)) {
-                    break;
-                } else {
-                    processedCommits.add(commitId);
-                }
-
-                // Write Commit/Revision Data
-                Commit commit = utilsService.getExpectedObject(commitId, commitFactory, conn);
-                commit.getModel().forEach(writer::handleStatement);
-
-                // Write Additions/Deletions Graphs
-                Difference revisionChanges = utilsService.getRevisionChanges(commitId, conn);
-                revisionChanges.getAdditions().forEach(writer::handleStatement);
-                revisionChanges.getDeletions().forEach(writer::handleStatement);
-            }
+            exportCommits(utilsService.getHeadCommitIRI(branch), processedCommits, writer, conn);
         });
+    }
+
+    protected void exportCommits(Resource headIRI, Set<Resource> processedCommits, ExportWriter writer, RepositoryConnection conn) {
+
+        // Write Commits
+        for (Resource commitId : utilsService.getCommitChain(headIRI, false, conn)) {
+
+            if (processedCommits.contains(commitId)) {
+                break;
+            } else {
+                processedCommits.add(commitId);
+            }
+
+            // Write Commit/Revision Data
+            Commit commit = utilsService.getExpectedObject(commitId, commitFactory, conn);
+            commit.getModel().forEach(writer::handleStatement);
+
+            // Write Additions/Deletions Graphs
+            Difference revisionChanges = utilsService.getRevisionChanges(commitId, conn);
+            revisionChanges.getAdditions().forEach(writer::handleStatement);
+            revisionChanges.getDeletions().forEach(writer::handleStatement);
+        }
     }
 }
---
+++ com.mobi.catalog.impl/src/test/java/com/mobi/catalog/impl/record/VersionedRDFRecordServiceTest.java
@@ -0,0 +1,182 @@
+package com.mobi.catalog.impl.record;
+
+/*-
+ * #%L
+ * com.mobi.catalog.impl
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2016 - 2018 iNovex Information Systems, Inc.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import com.mobi.catalog.api.CatalogProvUtils;
+import com.mobi.catalog.api.CatalogUtilsService;
+import com.mobi.catalog.api.builder.Difference;
+import com.mobi.catalog.api.ontologies.mcat.*;
+import com.mobi.catalog.api.record.config.RecordExportConfig;
+import com.mobi.jaas.api.ontologies.usermanagement.User;
+import com.mobi.ontologies.dcterms._Thing;
+import com.mobi.persistence.utils.BatchExporter;
+import com.mobi.persistence.utils.impl.SimpleSesameTransformer;
+import com.mobi.prov.api.ontologies.mobiprov.DeleteActivity;
+import com.mobi.rdf.api.IRI;
+import com.mobi.rdf.api.Model;
+import com.mobi.rdf.orm.OrmFactory;
+import com.mobi.rdf.orm.test.OrmEnabledTestCase;
+import com.mobi.repository.api.RepositoryConnection;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.BufferedGroupingRDFHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class VersionedRDFRecordServiceTest extends OrmEnabledTestCase {
+
+    private final IRI testIRI = VALUE_FACTORY.createIRI("urn:test");
+    private final IRI catalogId = VALUE_FACTORY.createIRI("http://mobi.com/test/catalogs#catalog-test");
+    private final IRI branchIRI = VALUE_FACTORY.createIRI("http://mobi.com/test/branches#branch");
+    private final IRI commitIRI = VALUE_FACTORY.createIRI("http://mobi.com/test/commits#commit");
+
+    private VersionedRDFRecordService recordService;
+    private SimpleSesameTransformer transformer;
+    private VersionedRDFRecord testRecord;
+    private Branch branch;
+    private Commit headCommit;
+    private Difference difference;
+    private User user;
+    private DeleteActivity deleteActivity;
+
+    private OrmFactory<VersionedRDFRecord> versionedRDFRecordFactory = getRequiredOrmFactory(VersionedRDFRecord.class);
+    private OrmFactory<Catalog> catalogFactory = getRequiredOrmFactory(Catalog.class);
+    private OrmFactory<User> userFactory = getRequiredOrmFactory(User.class);
+    private OrmFactory<DeleteActivity> deleteActivityFactory = getRequiredOrmFactory(DeleteActivity.class);
+    private OrmFactory<Branch> branchFactory = getRequiredOrmFactory(Branch.class);
+    private OrmFactory<Commit> commitFactory = getRequiredOrmFactory(Commit.class);
+
+    @Mock
+    private CatalogUtilsService utilsService;
+
+    @Mock
+    private RecordFactory recordFactory;
+
+    @Mock
+    private RepositoryConnection connection;
+
+    @Mock
+    private CatalogProvUtils provUtils;
+
+    @Before
+    public void setUp() throws Exception {
+
+        recordService = new VersionedRDFRecordService();
+        transformer = new SimpleSesameTransformer();
+        deleteActivity = deleteActivityFactory.createNew(VALUE_FACTORY.createIRI("http://test.org/activity/delete"));
+
+        user = userFactory.createNew(VALUE_FACTORY.createIRI("http://test.org/user"));
+        headCommit = commitFactory.createNew(commitIRI);
+        branch = branchFactory.createNew(branchIRI);
+        branch.setHead(headCommit);
+        branch.setProperty(VALUE_FACTORY.createLiteral("Test Record"), VALUE_FACTORY.createIRI(_Thing.title_IRI));
+
+        Model deletions = MODEL_FACTORY.createModel();
+        deletions.add(VALUE_FACTORY.createIRI("http://test.com#sub"), VALUE_FACTORY.createIRI(_Thing.description_IRI),
+                VALUE_FACTORY.createLiteral("Description"));
+
+        difference = new Difference.Builder()
+                .additions(MODEL_FACTORY.createModel())
+                .deletions(deletions)
+                .build();
+
+        testRecord = versionedRDFRecordFactory.createNew(testIRI);
+        testRecord.setProperty(VALUE_FACTORY.createLiteral("Test Record"), VALUE_FACTORY.createIRI(_Thing.title_IRI));
+        testRecord.setCatalog(catalogFactory.createNew(catalogId));
+        testRecord.setBranch(Collections.singleton(branch));
+
+
+        MockitoAnnotations.initMocks(this);
+        when(utilsService.getExpectedObject(any(IRI.class), any(OrmFactory.class), eq(connection))).thenReturn(testRecord);
+        when(utilsService.getBranch(eq(testRecord), eq(branchIRI), any(OrmFactory.class), eq(connection))).thenReturn(branch);
+        when(utilsService.getHeadCommitIRI(eq(branch))).thenReturn(commitIRI);
+        doReturn(Stream.of(commitIRI).collect(Collectors.toList()))
+                .when(utilsService).getCommitChain(eq(commitIRI), eq(false), any(RepositoryConnection.class));
+        when(utilsService.getExpectedObject(eq(commitIRI), any(OrmFactory.class), eq(connection))).thenReturn(headCommit);
+        when(utilsService.getRevisionChanges(eq(commitIRI), eq(connection))).thenReturn(difference);
+        when(provUtils.startDeleteActivity(any(User.class), any(IRI.class))).thenReturn(deleteActivity);
+
+        injectOrmFactoryReferencesIntoService(recordService);
+        recordService.setRecordFactory(recordFactory);
+        recordService.setUtilsService(utilsService);
+        recordService.setVf(VALUE_FACTORY);
+        recordService.setProvUtils(provUtils);
+    }
+
+    /* export() */
+
+    @Test
+    public void exportTest() throws Exception {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        RecordExportConfig config = new RecordExportConfig.Builder(os, RDFFormat.JSONLD, transformer).build();
+
+        BatchExporter exporter = config.getBatchExporter();
+        assertFalse(exporter.isActive());
+        recordService.export(testIRI, config, connection);
+        assertFalse(exporter.isActive());
+
+        verify(utilsService).getExpectedObject(eq(testIRI), any(OrmFactory.class), eq(connection));
+        verify(utilsService).getBranch(eq(testRecord), eq(branchIRI), any(OrmFactory.class), eq(connection));
+        verify(utilsService).getHeadCommitIRI(eq(branch));
+        verify(utilsService).getCommitChain(eq(commitIRI), eq(false), any(RepositoryConnection.class));
+        verify(utilsService).getExpectedObject(eq(commitIRI), any(OrmFactory.class), eq(connection));
+        verify(utilsService).getRevisionChanges(eq(commitIRI), eq(connection));
+    }
+
+    @Test
+    public void exportBatchExporterTest() throws Exception {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        BatchExporter exporter = new BatchExporter(transformer, new BufferedGroupingRDFHandler(Rio.createWriter(RDFFormat.JSONLD, os)));
+        RecordExportConfig config = new RecordExportConfig.Builder(exporter).build();
+
+        assertFalse(exporter.isActive());
+        exporter.startRDF();
+        assertTrue(exporter.isActive());
+        recordService.export(testIRI, config, connection);
+        exporter.endRDF();
+        assertFalse(exporter.isActive());
+
+        verify(utilsService).getExpectedObject(eq(testIRI), any(OrmFactory.class), eq(connection));
+        verify(utilsService).getBranch(eq(testRecord), eq(branchIRI), any(OrmFactory.class), eq(connection));
+        verify(utilsService).getHeadCommitIRI(eq(branch));
+        verify(utilsService).getCommitChain(eq(commitIRI), eq(false), any(RepositoryConnection.class));
+        verify(utilsService).getExpectedObject(eq(commitIRI), any(OrmFactory.class), eq(connection));
+        verify(utilsService).getRevisionChanges(eq(commitIRI), eq(connection));
+    }
+}
