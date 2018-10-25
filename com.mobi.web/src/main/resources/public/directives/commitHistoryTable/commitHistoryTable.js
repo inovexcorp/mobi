1ecc772199b3c0cfd24c906a82a98d0a306a881d
Tom Dalton <tomdalton@Toms-MacBook-Pro.local>
2018-10-25 13:38:25 -0600

    Updated issure with scope variable name.

--- com.mobi.web/src/main/resources/public/directives/commitHistoryTable/commitHistoryTable.js
+++ com.mobi.web/src/main/resources/public/directives/commitHistoryTable/commitHistoryTable.js
@@ -115,7 +115,7 @@
                     }
                     dvm.getCommits = function() {
                         if (dvm.commitId) {
-                            scope.$on('$destroy', function() {
+                            $scope.$on('$destroy', function() {
                                 httpService.cancel(dvm.id);
                             });
                             var promise = cm.getCommitHistory(dvm.commitId, dvm.targetId, dvm.id);
