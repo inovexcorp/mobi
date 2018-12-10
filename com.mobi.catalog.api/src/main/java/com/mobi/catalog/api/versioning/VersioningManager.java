71b741621e38f98100187e3084f1888d24794c91
Tom Dalton <tomdalton@Toms-MacBook-Pro.local>
2018-12-10 15:43:38 -0700

    Overloaded VersioningManager to pass in a RepositoryConnection to control when the connection is committed externally. Changed OntologyRecordService to use overloaded method.

--- com.mobi.catalog.api/src/main/java/com/mobi/catalog/api/versioning/VersioningManager.java
+++ com.mobi.catalog.api/src/main/java/com/mobi/catalog/api/versioning/VersioningManager.java
@@ -31,6 +31,7 @@ import com.mobi.catalog.api.ontologies.mcat.VersionedRDFRecord;
 import com.mobi.jaas.api.ontologies.usermanagement.User;
 import com.mobi.rdf.api.Model;
 import com.mobi.rdf.api.Resource;
+import com.mobi.repository.api.RepositoryConnection;
 
 public interface VersioningManager {
     /**
@@ -53,6 +54,26 @@ public interface VersioningManager {
                                                    String message, Model additions, Model deletions);
 
     /**
+     * Commits the provided addition and deletion {@link Model Models} for the provided {@link User} to
+     * the {@link Branch} identified by the provided Resource IDs with the
+     * provided message.
+     *
+     * @param catalogId The Resource identifying the Catalog which contains the Record.
+     * @param recordId The Resource identifying the VersionedRDFRecord which has the Branch.
+     * @param branchId The Resource identifying the Branch which will get the new Commit.
+     * @param user The User which will be associated with the new Commit.
+     * @param message The String with the message text associated with the new Commit.
+     * @param additions The statements which were added to the named graph.
+     * @param deletions The statements which were added to the named graph.
+     * @param conn A RepositoryConnection used by the VersioningService.
+     * @return The Resource of the new Commit.
+     * @throws IllegalArgumentException Thrown if the Catalog could not be found, the Record could not be found, the
+     *      Record does not belong to the Catalog, or the Branch could not be found.
+     */
+    Resource commit(Resource catalogId, Resource recordId, Resource branchId, User user,
+                    String message, Model additions, Model deletions, RepositoryConnection conn);
+
+    /**
      * Commits the {@link InProgressCommit} for the provided {@link User} to
      * the {@link Branch} identified by the provided Resource IDs with the
      * provided message.
@@ -66,8 +87,25 @@ public interface VersioningManager {
      * @throws IllegalArgumentException Thrown if the Catalog could not be found, the Record could not be found, the
      *      Record does not belong to the Catalog, the Branch could not be found, or the InProgress could not be found.
      */
-    Resource commit(Resource catalogId, Resource recordId, Resource branchId, User user,
-                                                   String message);
+    Resource commit(Resource catalogId, Resource recordId, Resource branchId, User user, String message);
+
+    /**
+     * Commits the {@link InProgressCommit} for the provided {@link User} to
+     * the {@link Branch} identified by the provided Resource IDs with the
+     * provided message.
+     *
+     * @param catalogId The Resource identifying the Catalog which contains the Record.
+     * @param recordId The Resource identifying the VersionedRDFRecord which has the Branch.
+     * @param branchId The Resource identifying the Branch which will get the new Commit.
+     * @param user The User with the InProgressCommit.
+     * @param message The String with the message text associated with the new Commit.
+     * @param conn A RepositoryConnection used by the VersioningService.
+     * @return The Resource of the new Commit.
+     * @throws IllegalArgumentException Thrown if the Catalog could not be found, the Record could not be found, the
+     *      Record does not belong to the Catalog, the Branch could not be found, or the InProgress could not be found.
+     */
+    Resource commit(Resource catalogId, Resource recordId, Resource branchId, User user, String message,
+                    RepositoryConnection conn);
 
     /**
      * Merges a {@link Branch} identified by the provided Resources into
@@ -88,6 +126,29 @@ public interface VersioningManager {
      * @throws IllegalArgumentException Thrown if the Catalog could not be found, the Record could not be found, the
      *      Record does not belong to the Catalog, or either Branch could not be found
      */
-    Resource merge(Resource catalogId, Resource recordId, Resource sourceBranchId,
-                                                  Resource targetBranchId, User user, Model additions, Model deletions);
+    Resource merge(Resource catalogId, Resource recordId, Resource sourceBranchId, Resource targetBranchId, User user,
+                   Model additions, Model deletions);
+
+    /**
+     * Merges a {@link Branch} identified by the provided Resources into
+     * another Branch identified by the provided Resources. Both Branches must belong to the same
+     * {@link VersionedRDFRecord}. The provided addition and deletion
+     * {@link Model Models} should resolve any conflicts and will be used to create the merge
+     * {@link Commit} which will be associated with the provided {@link User}.
+     * The head of the target Branch will be the new merge Commit, but the head of the source Branch will not change.
+     *
+     * @param catalogId The Resource identifying the Catalog which contains the Record.
+     * @param recordId The Resource identifying the VersionedRDFRecord which has the Branches.
+     * @param sourceBranchId The Resource identifying the source Branch which will merge into the target Branch.
+     * @param targetBranchId The Resource identifying the target Branch which will be merge into by the source Branch.
+     * @param user The User with the InProgressCommit.
+     * @param additions The statements which were added to the named graph.
+     * @param deletions The statements which were deleted from the named graph.
+     * @param conn A RepositoryConnection used by the VersioningService.
+     * @return The Resource of the new merge Commit.
+     * @throws IllegalArgumentException Thrown if the Catalog could not be found, the Record could not be found, the
+     *      Record does not belong to the Catalog, or either Branch could not be found
+     */
+    Resource merge(Resource catalogId, Resource recordId, Resource sourceBranchId, Resource targetBranchId, User user,
+                   Model additions, Model deletions, RepositoryConnection conn);
 }
