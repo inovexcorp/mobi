646550a56ed19457e1d5c184d2945b40eb09e848
Tom Dalton <tom.dalton@inovexcorp.com>
2019-02-15 10:07:29 -0700

    Restored explore service.

--- com.mobi.web/src/main/resources/public/discover/explore/explore.module.js
+++ com.mobi.web/src/main/resources/public/discover/explore/explore.module.js
@@ -20,9 +20,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-(function() {
-    'use strict';
-
-    angular
-        .module('explore', []);
-})();
+// (function() {
+//     'use strict';
+//
+//     angular
+//         .module('explore', []);
+// })();
