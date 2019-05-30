fb6019ddb324164cfa39108cffb092bac6af77bb
Ben Greenberg <ben.greenberg@inovexcorp.com>
2019-05-30 14:04:37 -0700

    Fixed unit test that tried to call a javascript function without parentheses

--- com.mobi.web/src/main/resources/public/ontology-editor/directives/conceptSchemesTab/conceptSchemesTab.directive.spec.js
+++ com.mobi.web/src/main/resources/public/ontology-editor/directives/conceptSchemesTab/conceptSchemesTab.directive.spec.js
@@ -72,11 +72,13 @@ describe('Concept Schemes Tab directive', function() {
         it('with a data-property-block depending on whether the selected entity is a concept', function() {
             ontologyManagerSvc.isConcept.and.returnValue(false);
             scope.$digest();
-            expect(this.element.find('data-property-block')).toBeFalsy;
+            console.log(JSON.stringify(this.element.find('datatype-property-block')));
+            expect(this.element.find('datatype-property-block').length).toBe(0);
 
             ontologyManagerSvc.isConcept.and.returnValue(true);
             scope.$digest();
-            expect(this.element.find('data-property-block')).toBeTruthy;
+            console.log(JSON.stringify(this.element.find('datatype-property-block')));
+            expect(this.element.find('datatype-property-block').length).toBe(1);
         })
         it('with a button to delete a concept scheme if a user can modify', function() {
             ontologyStateSvc.canModify.and.returnValue(true);
