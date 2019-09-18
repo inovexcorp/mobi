7e523512871c959a309b780a1c7059375dbe07c2
Khalil Savoy <khalil.savoy@inovexcorp.com>
2019-09-18 09:37:19 -0400

    initial creation of Mapping test for nightwatch

---
+++ mobi-itests/itests-web/src/test/js/Run_Mapping_Test.spec.js
@@ -0,0 +1,8 @@
+var adminUsername = "admin"
+var adminPassword = "admin"
+var Onto1 = process.cwd()+ '/src/test/resources/ontologies/test-local-imports-1.ttl'
+var Onto2 = process.cwd()+ '/src/test/resources/ontologies/test-local-imports-2.ttl'
+var Onto3 = process.cwd()+ '/src/test/resources/ontologies/test-local-imports-3.ttl'
+
+module.exports = {
+    '@tags': ['mobi', 'sanity', "ontology-editor"],
