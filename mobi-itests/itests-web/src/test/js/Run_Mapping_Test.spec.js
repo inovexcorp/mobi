ec3641fadea829f656f88f9b31d50d0b5a61212b
Khalil Savoy <khalil.savoy@inovexcorp.com>
2019-09-18 09:49:12 -0400

    Taken similar steps from Ontology Upload, added one line to combat race condition into Ontology_Upload_Test.spec.js

--- mobi-itests/itests-web/src/test/js/Run_Mapping_Test.spec.js
+++ mobi-itests/itests-web/src/test/js/Run_Mapping_Test.spec.js
@@ -1,8 +1,65 @@
+/*-
+ * #%L
+ * itests-web
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2016 - 2019 iNovex Information Systems, Inc.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 var adminUsername = "admin"
 var adminPassword = "admin"
-var Onto1 = process.cwd()+ '/src/test/resources/ontologies/test-local-imports-1.ttl'
-var Onto2 = process.cwd()+ '/src/test/resources/ontologies/test-local-imports-2.ttl'
-var Onto3 = process.cwd()+ '/src/test/resources/ontologies/test-local-imports-3.ttl'
+var OntoSample = process.cwd()+ '/src/test/resources/ontologies/test-local-imports-1.ttl'
+
 
 module.exports = {
     '@tags': ['mobi', 'sanity', "ontology-editor"],
+
+    'Step 1: login as admin' : function(browser) {
+        browser
+            .url('https://localhost:8443/mobi/index.html#/home')
+            .waitForElementVisible('input#username')
+            .waitForElementVisible('input#password')
+            .setValue('input#username', adminUsername)
+            .setValue('input#password', adminPassword)
+            .click('button[type=submit]')
+    },
+
+    'Step 2: check for visibility of home elements' : function(browser) {
+        browser
+            .waitForElementVisible('.home-page')
+    },
+
+    'Step 3: navigate to the Ontology Editor page' : function (browser) {
+        browser
+            .click('xpath', '//div//ul//a[@class="nav-link"][@href="#/ontology-editor"]')
+    },
+
+    'Step 4: click upload ontology' : function (browser) {
+        browser
+            .waitForElementVisible('div.btn-container button')
+            .click('xpath', '//div[@class="btn-container"]//button[text()[contains(.,"Upload Ontology")]]')
+    },
+
+    'Step 5: Upload and submit an Ontology' : function (browser) {
+        browser
+            .setValue('input[type=file]', OntoSample)
+            .waitForElementVisible('upload-ontology-overlay')
+            .click('xpath', '//button[text()[contains(.,"Submit")]]')
+    },
+
+
+}
