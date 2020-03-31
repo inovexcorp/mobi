081473e3708207a8045207642244ed313ce70f72
Stephen Kahmann <stephen.kahmann@inovexcorp.com>
2020-03-31 12:53:55 -0400

    Created test data

--- com.mobi.ontology.rest/src/test/java/com/mobi/ontology/rest/OntologyRestImplTest.java
+++ com.mobi.ontology.rest/src/test/java/com/mobi/ontology/rest/OntologyRestImplTest.java
@@ -1459,27 +1459,27 @@ public class OntologyRestImplTest extends MobiRestTestNg {
         assertEquals(responseObject.getJSONArray("noDomainProperties"), expectedResults);
     }
 
-//    @Test
-//    public void testGetOntologyStuffClassToAssociatedProperties() throws Exception {
-//        setupTupleQueryMock();
-//
-//        Model data = getModel("/getOntologyStuffData/ontologyData.ttl");
-//        JSONObject expectedResults = getResource("/getOntologyStuffData/classToAssociatedProperties-results.json");
-//
-//        try(RepositoryConnection conn = testQueryRepo.getConnection()) {
-//            conn.add(data);
-//        }
-//
-//        Response response = target().path("ontologies/" + encode(recordId.stringValue()) + "/ontology-stuff")
-//                .queryParam("branchId", branchId.stringValue()).queryParam("commitId", commitId.stringValue()).request()
-//                .get();
-//        JSONObject responseObject = getResponse(response);
-//
-//        assertEquals(response.getStatus(), 200);
-//        verify(ontologyManager).retrieveOntology(recordId, branchId, commitId);
-//        assertGetOntology(true);
-//        assertEquals(responseObject.getJSONObject("classToAssociatedProperties"), expectedResults);
-//    }
+    @Test
+    public void testGetOntologyStuffEntityNames() throws Exception {
+        setupTupleQueryMock();
+
+        Model data = getModel("/getOntologyStuffData/ontologyData.ttl");
+        JSONObject expectedResults = getResource("/getOntologyStuffData/entityNames-results.json");
+
+        try(RepositoryConnection conn = testQueryRepo.getConnection()) {
+            conn.add(data);
+        }
+
+        Response response = target().path("ontologies/" + encode(recordId.stringValue()) + "/ontology-stuff")
+                .queryParam("branchId", branchId.stringValue()).queryParam("commitId", commitId.stringValue()).request()
+                .get();
+        JSONObject responseObject = getResponse(response);
+
+        assertEquals(response.getStatus(), 200);
+        verify(ontologyManager).retrieveOntology(recordId, branchId, commitId);
+        assertGetOntology(true);
+        assertEquals(responseObject.getJSONObject("entityNames"), expectedResults);
+    }
 
     @Test
     public void testGetOntologyStuffEntityNamesNoResults() {
