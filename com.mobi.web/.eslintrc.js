3c97182faaef20d9d35a6087367056f79aa3deea
Robert Alvarez <robert.alvarez@inovexcorp.com>
2020-09-14 15:41:02 -0500

    slint setup

--- com.mobi.web/.eslintrc.js
+++ com.mobi.web/.eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
+    'standard': {
+      'env': [ 'karma' ],
+    },
     plugins: [
       '@typescript-eslint',
     ],
@@ -8,4 +11,21 @@ module.exports = {
       'eslint:recommended',
       'plugin:@typescript-eslint/recommended',
     ],
+    'rules': {
+      'eqeqeq': 'off',
+      'curly': 'error',
+      'quotes': ['error', 'single'],
+      // enforces spacing between keys and values in object literal properties
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      // require a space before & after certain keywords
+      'keyword-spacing': ['error', {
+        before: true,
+        after: true,
+        overrides: {
+          return: { after: true },
+          throw: { after: true },
+          case: { after: true }
+        }
+      }]
+    },
   };
