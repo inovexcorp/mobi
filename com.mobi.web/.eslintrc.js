5603f28a57cd0216e715bbe60165d8bdb884e947
Robert Alvarez <robert.alvarez@inovexcorp.com>
2020-09-30 09:20:52 -0500

    Update Eslint config file.

--- com.mobi.web/.eslintrc.js
+++ com.mobi.web/.eslintrc.js
@@ -1,31 +1,40 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    'standard': {
-      'env': [ 'karma' ],
-    },
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-    'rules': {
-      'eqeqeq': 'off',
-      'curly': 'error',
-      'quotes': ['error', 'single'],
-      // enforces spacing between keys and values in object literal properties
-      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-      // require a space before & after certain keywords
-      'keyword-spacing': ['error', {
-        before: true,
-        after: true,
-        overrides: {
-          return: { after: true },
-          throw: { after: true },
-          case: { after: true }
-        }
-      }]
-    },
-  };
+  root: true,
+  'env': {
+    'amd': true,
+    'browser': true,
+    'node': true,
+    'jasmine': true,
+    'angular/mocks': true
+  },
+  parser: '@typescript-eslint/parser',
+  globals: {
+    'angular': true
+  },
+  plugins: [
+    '@typescript-eslint',
+    'angular'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'rules': {
+    'eqeqeq': 'off',
+    'curly': 'error',
+    'quotes': ['error', 'single'],
+    // enforces spacing between keys and values in object literal properties
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+
+    'keyword-spacing': ['error', {
+      before: true,
+      after: true,
+      overrides: {
+        return: { after: true },
+        throw: { after: true },
+        case: { after: true }
+      }
+    }],
+    '@typescript-eslint/no-var-requires': 0,
+  },
+};
