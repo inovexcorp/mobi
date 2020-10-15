7a55d4042ed0b272167b8a3168a90dd731810dcd
Robert Alvarez <robert.alvarez@inovexcorp.com>
2020-10-15 16:21:17 -0500

    eslint-setup Update eslintrc rules.

--- com.mobi.web/.eslintrc.js
+++ com.mobi.web/.eslintrc.js
@@ -20,12 +20,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   'rules': {
-    'eqeqeq': 'off',
+    'eqeqeq': 'error',
     'curly': 'error',
     'quotes': ['error', 'single'],
     // enforces spacing between keys and values in object literal properties
     'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-
+    // enforce consistent spacing before and after keywords (keyword-spacing)
     'keyword-spacing': ['error', {
       before: true,
       after: true,
@@ -35,6 +35,27 @@ module.exports = {
         case: { after: true }
       }
     }],
+    // Disallows/allows the use of require statements except in import statements.
     '@typescript-eslint/no-var-requires': 0,
+    // Multiple empty lines
+    'no-multiple-empty-lines': [
+      'error', {
+        'max': 1,
+        'maxEOF': 0 
+      }
+    ],
+    //require or disallow semicolons
+    'semi': ['error', 'always'],
+    // opening curly brace of a block is placed on the same line as its corresponding statement or declaration
+    'brace-style': 2,
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-this-alias': [
+      'error',
+      {
+        'allowDestructuring': true, // Allow `const { props, state } = this`; false by default
+        'allowedNames': ['self'] // Allow `const self = this`; `[]` by default
+      }
+    ]
+
   },
 };
