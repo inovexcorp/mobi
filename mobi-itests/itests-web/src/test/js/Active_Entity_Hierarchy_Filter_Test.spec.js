aea3343fcb09c091171c1190e2516936e18d1213
Khalil Savoy <khalil.savoy@inovexcorp.com>
2020-10-28 11:55:59 -0400

    first pass at global functional tests

--- mobi-itests/itests-web/src/test/js/Active_Entity_Hierarchy_Filter_Test.spec.js
+++ mobi-itests/itests-web/src/test/js/Active_Entity_Hierarchy_Filter_Test.spec.js
@@ -20,8 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-var adminUsername = "admin"
-var adminPassword = "admin"
+
 var Onto1 = process.cwd()+ '/src/test/resources/ontologies/active-entity-filter-1.ttl'
 var Onto2 = process.cwd()+ '/src/test/resources/ontologies/active-entity-filter-2.ttl'
 var Onto3 = process.cwd()+ '/src/test/resources/ontologies/active-entity-filter-3.ttl'
@@ -29,34 +28,11 @@ var Onto3 = process.cwd()+ '/src/test/resources/ontologies/active-entity-filter-
 module.exports = {
     '@tags': ['sanity', "ontology-editor"],
 
-    'Step 1: login as admin' : function(browser) {
-        browser
-            .url('https://localhost:' +browser.globals.globalPort+ '/mobi/index.html#/home')
-            .waitForElementVisible('input#username')
-            .waitForElementVisible('input#password')
-            .setValue('input#username', adminUsername)
-            .setValue('input#password', adminPassword)
-            .click('button[type=submit]')
+    'Initial Setup: ' : function(browser) {
+        browser.globals.initial_steps(browser)
     },
 
-    'Step 2: check for visibility of home elements' : function(browser) {
-        browser
-            .waitForElementVisible('.home-page')
-    },
-
-    'Step 3: navigate to the Ontology Editor page' : function (browser) {
-        browser
-            .click('xpath', '//div//ul//a[@class="nav-link"][@href="#/ontology-editor"]')
-    },
-
-    'Step 4: click upload ontology' : function (browser) {
-        browser
-            .waitForElementNotPresent('div.spinner')
-            .waitForElementVisible('div.btn-container button')
-            .click('xpath', '//div[@class="btn-container"]//button[text()[contains(.,"Upload Ontology")]]')
-    },
-
-    'Step 5: Upload an Ontology' : function (browser) {
+    'Step 1: Upload an Ontology' : function (browser) {
         browser
             .setValue('input[type=file]', Onto1)
             .click('upload-ontology-overlay div.modal-footer button.btn')
@@ -67,13 +43,13 @@ module.exports = {
             .setValue('input[type=file]', Onto3)
     },
 
-    'Step 6: Submit all ontology files' : function (browser) {
+    'Step 2: Submit all ontology files' : function (browser) {
         browser
             .waitForElementVisible('upload-ontology-overlay')
             .click('xpath', '//button[text()[contains(.,"Submit All")]]')
     },
 
-    'Step 7: Validate Ontology Appearance' : function (browser) {
+    'Step 3: Validate Ontology Appearance' : function (browser) {
         browser
             .waitForElementVisible('div.ontologies')
             .assert.elementNotPresent('div.modal-header')
@@ -85,18 +61,18 @@ module.exports = {
             .useCss()
     },
 
-    'Step 8: Click on Ontology called active-entity-filter-1.ttl' : function (browser) {
+    'Step 4: Click on Ontology called active-entity-filter-1.ttl' : function (browser) {
         browser
             .click('xpath', '//div[contains(@class, "list-group")]//div//div[text()[contains(.,"active-entity-filter-1.ttl")]]')
     },
 
-    'Step 9: Click classes tab' : function (browser) {
+    'Step 5: Click classes tab' : function (browser) {
         browser
             .waitForElementVisible('div.material-tabset li.nav-item')
             .click('xpath', '//div[contains(@class, "material-tabset")]//li[contains(@class, "nav-item")]//span[text()[contains(., "Classes")]]')
     },
 
-    'Step 10: Check for Ontology classes' : function (browser) {
+    'Step 6: Check for Ontology classes' : function (browser) {
         browser
             .waitForElementVisible('div.tree')
             .useXpath()
@@ -107,7 +83,7 @@ module.exports = {
             .assert.elementNotPresent({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Class 1")]]'})
     },
 
-    'Step 11: Click on an imported class' : function (browser) {
+    'Step 7: Click on an imported class' : function (browser) {
         browser
             .useCss()
             .click('xpath', '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Other Class")]]//parent::a')
@@ -116,7 +92,7 @@ module.exports = {
             .assert.containsText('selected-details .entity-name', 'Other Class')
     },
 
-    'Step 12: Apply the Active Entity Filter' : function (browser) {
+    'Step 8: Apply the Active Entity Filter' : function (browser) {
         browser
             .waitForElementVisible('.hierarchy-filter a')
             .click('.hierarchy-filter a')
@@ -127,29 +103,29 @@ module.exports = {
             .waitForElementNotVisible('class-hierarchy-block .dropdown-menu checkbox')
     },
 
-    'Step 13: Ensure that imported entities have been filtered out' : function(browser) {
+    'Step 9: Ensure that imported entities have been filtered out' : function(browser) {
         browser
             .waitForElementNotPresent({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Class 3")]]'})
             .waitForElementNotPresent({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Other Class")]]'})
     },
 
-    'Step 14: Ensure that all active entities are visible' : function(browser) {
+    'Step 10: Ensure that all active entities are visible' : function(browser) {
         browser
             .waitForElementVisible({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Class 1")]]'})
             .assert.visible({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Class 0")]]'})
     },
 
-    'Step 15: Ensure that imported parents of active entities are visible' : function(browser) {
+    'Step 11: Ensure that imported parents of active entities are visible' : function(browser) {
         browser
             .assert.visible({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Class 2")]]'})
     },
 
-    'Step 16: Ensure the selected entity view is still visible to the user, even if the entity is filtered out of the active list.': function(browser) {
+    'Step 12: Ensure the selected entity view is still visible to the user, even if the entity is filtered out of the active list.': function(browser) {
         browser
             .assert.containsText('selected-details .entity-name', 'Other Class')
     },
 
-    'Step 17: Remove the Active Entity filter' : function(browser) {
+    'Step 13: Remove the Active Entity filter' : function(browser) {
         browser
             .click('.hierarchy-filter a')
             .waitForElementVisible('class-hierarchy-block .dropdown-menu checkbox')
@@ -160,7 +136,7 @@ module.exports = {
 
     },
 
-    'Step 18: Verify the Active Entity filtered state was applied to the pre-filtered state' : function(browser) {
+    'Step 14: Verify the Active Entity filtered state was applied to the pre-filtered state' : function(browser) {
         browser
             .useXpath()
             .waitForElementVisible({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Class 0")]]'})
@@ -170,7 +146,7 @@ module.exports = {
             .waitForElementVisible({locateStrategy: 'xpath', selector: '//div[contains(@class, "tree-item-wrapper")]//span[text()[contains(., "Class 1")]]'})
     },
 
-    'Step 19: Verify that a message is displayed when no entities match the filter criteria' : function(browser) {
+    'Step 15: Verify that a message is displayed when no entities match the filter criteria' : function(browser) {
         browser
             .useCss()
             .assert.visible('search-bar input')
@@ -183,9 +159,5 @@ module.exports = {
             .waitForElementNotVisible('class-hierarchy-block .dropdown-menu checkbox')
             .waitForElementVisible('info-message span')
             .assert.containsText('info-message span', 'No entities match your filter.')
-    } 
-
-
-
-
+    }
 }
--- mobi-itests/itests-web/src/test/resources/filtered-resources/nightwatch-globals.js
+++ mobi-itests/itests-web/src/test/resources/filtered-resources/nightwatch-globals.js
@@ -1,3 +1,25 @@
 module.exports = {
-  "globalPort" : "${https-port}"
+  "globalPort" : "${https-port}",
+
+  "initial_steps" : function (browser) {
+      var adminUsername = "admin"
+      var adminPassword = "admin"
+
+      browser
+          .url('https://localhost:' + browser.globals.globalPort + '/mobi/index.html#/home')
+          .waitForElementVisible('input#username')
+          .waitForElementVisible('input#password')
+          .setValue('input#username', adminUsername)
+          .setValue('input#password', adminPassword)
+          .click('button[type=submit]')
+          .waitForElementVisible('.home-page')
+          .click('xpath', '//div//ul//a[@class="nav-link"][@href="#/ontology-editor"]')
+          .waitForElementNotPresent('div.spinner')
+          .waitForElementVisible('div.btn-container button')
+          .click('xpath', '//div[@class="btn-container"]//button[text()[contains(.,"Upload Ontology")]]')
+  },
+
+  "cleanup_steps" : function (browser) {
+      browser
+  }
 }
