bf1c7cffac042cb106bbfea42a87793520ce536c
Ben Greenberg <ben.greenberg@LPT-2014.local>
2020-10-29 12:54:19 -0400

    fix build error

--- mobi-itests/itests-web/src/test/js/Case_Sensitivity_Test.spec.js
+++ mobi-itests/itests-web/src/test/js/Case_Sensitivity_Test.spec.js
@@ -20,6 +20,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
+var adminUsername = "admin"
+var adminPassword = "admin"
 
 module.exports = {
   '@tags': ['login', 'sanity'],
