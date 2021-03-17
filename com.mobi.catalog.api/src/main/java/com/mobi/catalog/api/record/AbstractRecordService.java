317e8c629923cf0a660e8f7c72bbedcc211398b5
Tom Dalton <tom.dalton@inovexcorp.com>
2021-03-17 16:01:54 -0600

    Reworked to use OSGi annotation.

--- com.mobi.catalog.api/src/main/java/com/mobi/catalog/api/record/AbstractRecordService.java
+++ com.mobi.catalog.api/src/main/java/com/mobi/catalog/api/record/AbstractRecordService.java
@@ -45,6 +45,7 @@ import com.mobi.rdf.orm.OrmFactory;
 import com.mobi.repository.api.RepositoryConnection;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.rdf4j.model.vocabulary.DCTERMS;
+import org.osgi.service.component.annotations.Reference;
 
 import java.time.OffsetDateTime;
 import java.util.Set;
@@ -58,11 +59,19 @@ import java.util.stream.Collectors;
  */
 public abstract class AbstractRecordService<T extends Record> implements RecordService<T> {
 
+    @Reference
     protected CatalogProvUtils provUtils;
+
+    @Reference
     protected CatalogUtilsService utilsService;
-    protected OrmFactory<T> recordFactory;
+
+    @Reference
     protected ValueFactory valueFactory;
+
+    @Reference
     protected CatalogFactory catalogFactory;
+
+    protected OrmFactory<T> recordFactory;
 
     @Override
     public T create(User user, RecordOperationConfig config, RepositoryConnection conn) {
