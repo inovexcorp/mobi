7a9126971954eb46efa3c01b8868fd3242189cce
Robert Alvarez <robert.alvarez@realmone.com>
2024-08-30 16:56:01 +0000

    change karma.conf.build.js

--- com.mobi.web/karma.conf.build.js
+++ com.mobi.web/karma.conf.build.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
         require('@angular-devkit/build-angular/plugins/karma')
       ],
       client: {
-        clearContext: false, // leave Jasmine Spec Runner output visible in browser
+        clearContext: true, // leave Jasmine Spec Runner output visible in browser
         args: ['--build']
       },
       coverageIstanbulReporter: {
